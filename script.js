ba9c91d7783765215e7690eb23aef5d5031853ae
juniomelos <juniomelos@gmail.com>
2020-04-06 19:11:27 -0300

    text space, image and borders - ok

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let memeContainer;
 window.onload = function(){
-  
+  memeContainer = document.getElementById("meme-image-container");
 }
+function addImageContainer (event){
+  let img = document.createElement("img");
+  img.src = URL.createObjectURL(event.target.files[0])
+  memeContainer.appendChild(img);
+}
+let textInput = document.getElementById("text-input");
+let memeText = document.getElementById("meme-text");
+textInput.addEventListener("input", function(){
+  memeText.innerHTML= textInput.nodeValue;
+})
+
+
