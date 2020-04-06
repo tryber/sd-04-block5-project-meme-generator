8763da74ded1db85d7f2acd2f181eabdbd216f91
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-06 19:16:26 -0300

    Alterada forma de leitura da imagem.'

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let userImage = document.getElementById('text-input');
-let txt = document.querySelector('div#meme-image-container p');
+const userImage = document.getElementById('text-input'); //imagem do input file.
+const container = document.getElementById('meme-image-container'); // container da imagem.
+const txt = document.querySelector('div#meme-image-container p'); // tag p da imagem.
+let img; //
 
-userImage.addEventListener('change', addImage);
+userImage.addEventListener('change', addImage); // evento para o input file.
 
-function addImage() {
-    const img = document.querySelector('img');
-    img.style.display = 'inline';
-    txt.style.display = 'block'
+function addImage(event) {
+    if(!img){
+        img = document.createElement('img');
+    }
     img.src = URL.createObjectURL(event.target.files[0]);
+    container.appendChild(img);
+    img.style.display = 'inline';
+    //txt.style.display = 'block'
 }
