576c44f80a9701b66cf5036c88033478201ef154
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-06 17:40:25 -0300

    Tentativas infifitas de ler a imagem e settar o src.'

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function addImage() {
+    const userImage = document.getElementById('user-image');
+    const memeImageContainer = document.getElementById('meme-image-container');
+    const img = memeImageContainer.querySelector('img');
     
+    userImage.addEventListener('change', function(event) {
+        //const file = this.files[0];
+        alert('Teste');
+        img.src = URL.createObjectURL(this.files[0].name);
+    });
 }
