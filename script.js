9c0686b3c7f1a0b48b0cd391fb5cad9e3b08b942
Jorge Osiro <jojimurakabe@gmail.com>
2020-04-06 18:24:18 -0300

    events

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ textInput.addEventListener('keyup', function(){
     let paragraph = document.querySelector(".meme-text");
     paragraph.innerHTML = textInput.value;
 })
+
+let submit = document.querySelector("#meme-insert")
+submit.addEventListener('change', function(){
+    let image = document.querySelector("#meme-image")
+    let file = this.files[0];
+
+    if(file) {
+        let reader = new FileReader();
+        image.style.display = "block";
+        reader.addEventListener("load", function(){
+            image.setAttribute("src", this.result)
+        })
+        reader.readAsDataURL(file);
+    }else {
+        image.style.display = null;
+        image.setAttribute("src", "")
+    }
+})
