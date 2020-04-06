5fb86d240bf07c4e7cc4b6dff948053a83222e93
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-06 18:47:50 -0300

    Texto sobre imagem.'

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let userImage = document.getElementById('text-input');
+let txt = document.querySelector('div#meme-image-container p');
+
+userImage.addEventListener('change', addImage);
+
 function addImage() {
     const img = document.querySelector('img');
-    img.style.display = 'block';
+    img.style.display = 'inline';
+    txt.style.display = 'block'
     img.src = URL.createObjectURL(event.target.files[0]);
 }
