daf184fab8e9feab06c467a818d48c2890a75039
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-06 19:42:48 -0300

    text input done

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 let memeContainer;
-window.onload = function(){
+let img;
+let txts = [];
+window.onload = function () {
 	memeContainer = document.getElementById("meme-image-container");
 }
-let img;
-function addImageToContainer(event){
-	if (!img){
+function addImageToContainer(event) {
+	if (!img) {
 		img = document.createElement("img");
-		img.style.width = "100%";	
-		memeContainer.appendChild(img);
+		img.style.width = "100%";
+		memeContainer.prepend(img);
 	}
 	img.src = URL.createObjectURL(event.target.files[0]);
-	img.onload = function(){
+	img.onload = function () {
 		memeContainer.style.height = img.clientHeight + "px";
+	}
+}
+
+function addTextToContainer(event, index) {
+	let text = txts[index];
+	if (!text) {
+		text = document.createElement("p");
+		txts[index] = text;
+		memeContainer.appendChild(text);
+	}
+	text.innerHTML = event.target.value;
+}
+
+function changeTextColor(event) {
+	for (let i in txts) {
+		txts[i].style.color = event.target.value;
 	}
 }
