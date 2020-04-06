c459efb85ac33ce5b79b19a753fe16f3f3e025d0
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-06 16:05:59 -0300

    step1

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   let container = document.getElementById("meme-image-container");
   let text = document.createElement("p");
-  text.id = "text-input"
   container.appendChild(text);
-  let inputText = document.getElementById("input");
+  let inputText = document.getElementById("text-input");
+  // input text dynamically over the image
   inputText.addEventListener("input", function(){
     text.innerHTML = inputText.value;
   })
