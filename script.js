763687bc5eded100a111536022c24b244184e868
orlandomessias <programadorlando@gmail.com>
2020-04-06 17:28:59 -0300

    script set image into div

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let textInput = document.getElementById('text-input')
 
+textInput.addEventListener('keyup', ()=>{
+    document.getElementById('meme-text').innerHTML = textInput.value
+})
+
+function setImage(){
+    let inputFile = document.getElementById('meme-insert').files[0]
+    let img = document.getElementById('meme-image')
+    let fr = new FileReader()
+    fr.onload = function(){
+        img.src = fr.result
+    }
+    if(inputFile)
+        fr.readAsDataURL(inputFile)
+    else
+        img.src = ''
+}
