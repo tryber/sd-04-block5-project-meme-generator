caabd6aa51a5f499153caf653f7e4f9eb34d9962
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-06 18:01:31 -0300

    moved style and script to extexternal files

---
+++ script.js
@@ -0,0 +1,11 @@
+let memeText = document.getElementById("memeText")
+let textInput = document.getElementById("text-input")
+let memeImg = document.getElementById("meme-image")
+let imageInput = document.getElementById("meme-insert")
+
+imageInput.addEventListener("change", function(){
+    memeImg.src = URL.createObjectURL(imageInput.files[0])
+})
+textInput.addEventListener("keyup", function(){
+    memeText.innerText = textInput.value
+})
