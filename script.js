b3f7bf7253ad69b2940816b28492c99b7f3662fa
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-06 15:48:52 -0300

    Add meme-insert

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-window.onload = function() {
-    
+window.onload = function () {
+
     let textInput = this.document.getElementById("text-input");
     let textMeme = this.document.getElementById("text-meme");
-    this.console.log(textInput.value)
+    let memeInput = this.document.getElementById("meme-insert");
+    let memeImage = this.document.getElementById("meme-image");
 
-    textInput.addEventListener("keyup", function() {
+    textInput.addEventListener("keyup", function () {
         textMeme.innerHTML = textInput.value;
     })
+
+    memeInput.addEventListener("change", function() {
+        memeImage.src = URL.createObjectURL(this.files[0]);
+    })
 }
+
