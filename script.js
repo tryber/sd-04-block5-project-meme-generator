f8182529ac2511883669e9a40bf2d0575f3c927e
Leandro Marten <leandromarten1@gmail.com>
2020-04-06 16:10:15 -0300

    primeiros passos

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+
+let textInput = document.querySelector('#text-input');
+let imgInput = document.querySelector('#meme-insert');
+let btn =  document.querySelector('#btnGerar');
+
+
+btn.addEventListener('click', function(){
+  textInput
+
+});
+
+
+
+
+};
