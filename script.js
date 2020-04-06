2700ccc2583b1067f155d2cce7d22895e6bea9f8
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-06 19:22:40 -0300

    changed event listener function

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let textInput = document.getElementById("text-input")
 let memeImg = document.getElementById("meme-image")
 let imageInput = document.getElementById("meme-insert")
 
-imageInput.addEventListener("change", function(){
-    memeImg.src = URL.createObjectURL(imageInput.files[0])
+imageInput.addEventListener("change", function(e){
+    memeImg.src = URL.createObjectURL(e.target.files[0])
 })
 textInput.addEventListener("keyup", function(){
     memeText.innerText = textInput.value
