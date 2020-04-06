9dccbd55fc0aaad95a54eba7200d8fddafd2ce20
danilossousa <danilosantossousa@gmail.com>
2020-04-06 17:40:30 -0300

    Teste 3 requisito 1

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = function () {
+  let inpTxt = document.getElementById("text-input");
+  let memeTxt = document.getElementById("meme-text");
 
+  //console.log(inpTxt.value)
+
+  function pasteTxt() {
+    memeTxt.innerText = inpTxt.value;
+  };
+
+  inpTxt.addEventListener("input", pasteTxt);
 };
