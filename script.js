dc2f011f5b5c92849b8756a2e81c7a048296ca0c
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-04-06 17:17:29 -0300

    add buttons

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ window.onload = function () {
     let textMeme = this.document.getElementById("text-meme");
     let memeInput = this.document.getElementById("meme-insert");
     let memeImage = this.document.getElementById("meme-image");
+    let meme = this.document.getElementById("meme-image-container");
+    let button1 = this.document.getElementById("button1");
+    let button2 = this.document.getElementById("button2");
+    let button3 = this.document.getElementById("button3");
 
     textInput.addEventListener("keyup", function () {
         textMeme.innerHTML = textInput.value;
@@ -12,4 +16,21 @@ window.onload = function () {
     memeInput.addEventListener("change", function () {
         memeImage.src = URL.createObjectURL(this.files[0]);
     })
+
+    button1.addEventListener("click", function() {
+        meme.style.border = "3px dashed red"
+    })
+
+    button2.addEventListener("click", function() {
+        meme.style.border = "5px double blue"
+    })
+
+    button3.addEventListener("click", function() {
+        meme.style.border = "6px groove green"
+    })
 }
+
+function reset() {
+    document.getElementById("meme-image-container").style.border = "1px solid black"
+}
+
