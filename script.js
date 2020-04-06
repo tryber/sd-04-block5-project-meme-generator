ead94c24892063601215a36a0c5975c94e9dee56
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-06 16:57:53 -0300

    Funcao para buscar imagem e funcao para inserir o texto

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let a = 3
-console.log(a)
+let button = document.querySelector('#upload-image');
+let memeImg = document.querySelector('#meme-image');
+let memeInsert = document.querySelector('#meme-insert');
+let textInput = document.querySelector('#text-input');
+let textMeme = document.querySelector('#text-meme');
+
+
+memeInsert.addEventListener("change", function(){
+  memeImg.src = URL.createObjectURL(this.files[0]);
+})
+
+textInput.addEventListener('keyup', function(){
+  textMeme.innerText = textInput.value
+})
+
+
+
+
+
+
