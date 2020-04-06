2246b37581c090aba57d7e2a7aa68fd9ed0cfbad
orlandomessias <programadorlando@gmail.com>
2020-04-06 18:00:21 -0300

    script set image into div new features

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function setImage(){
     let inputFile = document.getElementById('meme-insert').files[0]
     let img = document.getElementById('meme-image')
     let fr = new FileReader()
+    let memeImageContainer = document.getElementById('meme-image-container')
     fr.onload = function(){
         img.src = fr.result
     }
+    
     if(inputFile)
         fr.readAsDataURL(inputFile)
     else
