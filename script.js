851bdc86e41b372df0d34f3ce0110ca17cd4da54
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-06 15:46:23 -0300

    step1

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 window.onload = function() {
   let container = document.getElementById("meme-image-container");
   let text = document.createElement("p");
+  text.id = "text-input"
   container.appendChild(text);
-  let inputText = document.getElementById("text-input");
+  let inputText = document.getElementById("input");
   inputText.addEventListener("input", function(){
     text.innerHTML = inputText.value;
   })
