ad3599baf5c049e2e1c6eeebe49894e10a65baab
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-06 20:32:42 -0300

    Add p tag on upper text to try pass cypress

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.onload = function() {
   const upperTextInput = document.querySelector('#text-input');
   upperTextInput.addEventListener('input', function(e) {
     const upperText = document.querySelector('#upper-text');
-    upperText.innerHTML = e.target.value;
+    upperText.querySelector('p').innerHTML = e.target.value;
   });
 };
