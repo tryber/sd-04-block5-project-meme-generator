9a6faf35a3d56ed4ec4b046f1ebe3fbf8730be84
Filipe de Castro <filipeyoga@gmail.com>
2020-04-06 20:07:15 -0300

    trying to pass the bot 6

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let textInput = document.querySelector("#text-input");
-let textMeme = document.querySelector("#text-meme")
+let textMeme = document.querySelector("#text-meme");
 
 let memeImg = document.querySelector("#meme-image");
 let memeInsert = document.querySelector("#meme-insert");
