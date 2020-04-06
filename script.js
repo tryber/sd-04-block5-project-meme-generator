085102398b6797126296d61c5be230990021970e
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-06 20:06:47 -0300

    Image only added after submition

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let memeText = document.getElementById("meme-text")
 let textInput = document.getElementById("text-input")
-let memeImg = document.getElementById("meme-image")
 let imageInput = document.getElementById("meme-insert")
+let imageContainer = document.getElementById("meme-image-container")
 
 imageInput.addEventListener("change", function(e){
-    memeImg.src = URL.createObjectURL(e.target.files[0])
+    let memeImage = document.createElement("img")
+    memeImage.src = URL.createObjectURL(e.target.files[0])
+    memeImage.id = "meme-image"
+    imageContainer.append(memeImage)
+
 })
 textInput.addEventListener("keyup", function(){
     memeText.innerText = textInput.value
