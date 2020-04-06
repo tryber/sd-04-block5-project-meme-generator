f67209b8a454535d2df16d48fbbd7ce4eefd89ac
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-06 18:37:02 -0300

    uploding now working

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let memeContainer;
 window.onload = function(){
-	
+	memeContainer = document.getElementById("meme-image-container");
+}
+let img;
+function addImageToContainer(event){
+	if (!img){
+		img = document.createElement("img");
+		img.style.width = "100%";	
+		memeContainer.appendChild(img);
+	}
+	img.src = URL.createObjectURL(event.target.files[0]);
+	img.onload = function(){
+		memeContainer.style.height = img.clientHeight + "px";
+	}
 }
