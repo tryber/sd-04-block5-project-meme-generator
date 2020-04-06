3f588c78824233d16ee164dc08d34b78c5bdb844
binhus <eng.robsonwilli@gmail.com>
2020-04-06 20:27:59 -0300

    remove text on the bottom and add the script to run the page - able to the first pull request

---
+++ script.js
@@ -0,0 +1,27 @@
+const inpFile = document.getElementById("meme-image");
+const topText = document.querySelector("#text-input");
+const bottomText = document.querySelector("#text-input2");
+const container = document.getElementById("meme-image-container");
+const image = container.querySelector(".image-container");
+let ImageText = topText.value;
+
+inpFile.addEventListener("change",function(){
+    const file =this.files[0];
+    console.log(file);
+
+    if (file){
+        const reader = new FileReader();
+
+        image.style.display = "block";
+
+        reader.addEventListener("load",function(){
+            image.setAttribute("src",this.result);
+        });
+        reader.readAsDataURL(file);
+    }
+});
+
+topText.addEventListener("change", function(){
+    ImageText = "<p>"+topText.value+"</p>";
+    document.querySelector('#text').innerHTML = ImageText;
+});
