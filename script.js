4026167b5844668732d9ef7ffb711b923dad1bb0
ederu <ederu@debian>
2020-04-06 18:15:33 -0300

    MG v0.9 - Parte 1

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let textInput = document.getElementById('text-input');
 let text = document.getElementById('meme-text');
 
-textInput.addEventListener('input', () => { text.innerHTML = textInput.value })
+textInput.addEventListener('input', function(){
+    text.innerHTML = textInput.value }
+)
 
 let imgInput = document.getElementById('meme-insert');
 let img = document.getElementById('meme-image');
 
-imgInput.addEventListener('input', () => {
+imgInput.addEventListener('input', function(){
     console.log(imgInput.files[0].name);
     img.src = URL.createObjectURL(event.target.files[0]);
 });
@@ -16,13 +18,13 @@ let btn2 = document.getElementById('button2');
 let btn3 = document.getElementById('button3');
 let div = document.getElementById('meme-image-container');
 
-btn1.addEventListener('click', ()=>{
+btn1.addEventListener('click', function(){
     div.style.border='3px dashed red';
 });
-btn2.addEventListener('click', ()=>{
+btn2.addEventListener('click', function(){
     div.style.border='5px double blue';
 });
-btn3.addEventListener('click', ()=>{
+btn3.addEventListener('click', function(){
     div.style.border='6px groove green';
 });
 
@@ -31,15 +33,15 @@ let btn5 = document.getElementById('meme-2');
 let btn6 = document.getElementById('meme-3');
 let btn7 = document.getElementById('meme-4');
 
-btn4.addEventListener('click', ()=>{
+btn4.addEventListener('click', function(){
     img.src='./imgs/meme1.jpg';
 });
-btn5.addEventListener('click', ()=>{
+btn5.addEventListener('click', function(){
     img.src='./imgs/meme2.jpg';
 });
-btn6.addEventListener('click', ()=>{
+btn6.addEventListener('click', function(){
     img.src='./imgs/meme3.jpg';
 });
-btn7.addEventListener('click', ()=>{
+btn7.addEventListener('click', function(){
     img.src='./imgs/meme4.jpg';
 });
