9ba839c5745f37102d39e1cade94f377c2e956d2
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-06 19:04:45 -0300

    add function text displayed

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.onload = function(){
+    const caixaTexto = document.querySelector('#text-input');
+    const textdisplayed = document.querySelector('#textToDisplay');
+    caixaTexto.addEventListener('keyup', trocaTexto);
 
-
- }
+    function trocaTexto () {
+        textdisplayed.innerText = caixaTexto.value;
+      
+      } 
+}
 
  
