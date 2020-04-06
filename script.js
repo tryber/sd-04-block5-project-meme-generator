f66f26fc525ca5b3e1b81d9a88e1760b570ba66c
Filipe de Castro <filipeyoga@gmail.com>
2020-04-06 17:17:43 -0300

    updated script

---
+++ script.js
@@ -0,0 +1,13 @@
+let textInput = document.querySelector("#text-input");
+let textMeme = document.querySelector("#text-meme")
+
+let memeImg = document.querySelector("#meme-image");
+let memeInsert = document.querySelector("#meme-insert");
+
+memeInsert.addEventListener("change", function () {
+  memeImg.src = URL.createObjectURL(this.files[0]);
+});
+
+textInput.addEventListener("keyup", function () {
+  textMeme.innerText = textInput.value;
+});
