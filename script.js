8399b0b9ed94b1bb00cd8565526fedc140006fde
Leandro Marten <leandromarten1@gmail.com>
2020-04-06 16:55:17 -0300

    estrutura HTML, CSS e inicio JS

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 window.onload = function(){
 
-let textInput = document.querySelector('#text-input');
-let imgInput = document.querySelector('#meme-insert');
-let btn =  document.querySelector('#btnGerar');
-
-
-btn.addEventListener('click', function(){
-  textInput
-
-});
+  let textInput = document.querySelector('#text-input');
+  let imgInput = document.querySelector('#meme-insert');
+  let btn =  document.querySelector('#btnGerar');
+  let container = document.querySelector('#meme-image-container');
+  let memeText = document.querySelector('#meme-text');
+  let memeImg = previewContainer.querySelector('.meme-img');
+  
+  
+  textInput.addEventListener('keyup', function(){
+    memeText.innerHTML = textInput.value;
+  
+  });
+  
+  
+  // btn.addEventListener('click', function(){
+  
+  // });
 
 
 
