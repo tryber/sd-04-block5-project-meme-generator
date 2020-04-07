cdb00fb3cb1b5c71a96eb206c9c4c78bc22caf6e
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-06 21:19:57 -0300

    Revert "Remove text-input elemente from containers to try pass cypress"
    This reverts commit 5bddeb142eaaf1aaa001b5ade049f6850a82bafa.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = function() {
-  const upperTextInput = document.querySelector('#text-input');
-  upperTextInput.addEventListener('input', function(e) {
-    const upperText = document.querySelector('#upper-text');
-    upperText.innerHTML = e.target.value;
-  });
-};
+window.onload = function () {
+  const upperTextInput = document.querySelector('#text-input')
+  upperTextInput.addEventListener('input', function (e) {
+    const upperText = document.querySelector('#upper-text')
+    upperText.innerHTML = e.target.value
+  })
+}
