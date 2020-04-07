eae6fdf030b76211e1e3bbb507dc5495c39603e3
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-07 19:27:24 -0300

    feature/added text input and img upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let textInput = document.querySelector('#text-input');
+let imageContainer = document.querySelector('#meme-image-container');
+let memeText = document.querySelector('#meme-text');
+let memeInsert = document.querySelector('#meme-insert');
+
+textInput.addEventListener('keyup', () => {
+  memeText.innerHTML = textInput.value;
+});
+
+memeInsert.addEventListener('change', () => {
+  let img = document.createElement('img');
+  img.id = 'meme-image';
+  imageContainer.appendChild(img);
+  img.src = URL.createObjectURL(event.target.files[0]);
+});
