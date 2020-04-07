27ad67d0211984b523593a32b7585ba6645ada58
joaosoutto <joaosoutto92@gmail.com>
2020-04-07 01:36:16 -0300

    tentativa de finalizar 2

--- script.js
+++ script.js
@@ -1,95 +1,53 @@
 window.onload = function(){
-
     //função de adicionar o texto!
     let memeText = document.getElementById('text-input');
     let text = document.getElementById('meme-text');
-
     function inputText(){
-        text.innerHTML = memeText.value
-        };
-
+    text.innerHTML = memeText.value
+    };
     memeText.addEventListener('input', inputText);
-    //função de adicionar o texto!
-    //-----------------------------------------------------------------------------------------------------
     //função de adicionar imagem!
     let memeInput = document.getElementById('meme-insert');
     let meme = document.getElementById('meme-image');
-    
     function imgInput(){
-        meme.src = URL.createObjectURL(event.target.files[0]);
-        };
-
+    meme.src = URL.createObjectURL(event.target.files[0]);
+    };
     memeInput.addEventListener('input', imgInput);
-    //função de adicionar imagem!
-    //-----------------------------------------------------------------------------------------------------
     //função de alterar margens!------------------------------------------
-    
     let b1 = document.getElementById('button1');
     function border1(){
-        memeDiv.style.border='3px dashed red';
+    memeDiv.style.border='3px dashed red';
     }
     b1.addEventListener('click', border1);
-
     let b2 = document.getElementById('button2');
     function border2(){
-        memeDiv.style.border='5px double blue';
+    memeDiv.style.border='5px double blue';
     }
     b2.addEventListener('click', border2);
-
     let b3 = document.getElementById('button3');
     function border3(){
-        memeDiv.style.border='6px groove green';
+    memeDiv.style.border='6px groove green';
     }
     b3.addEventListener('click', border3);
-
     let b4 = document.getElementById('meme-1');
     function meme1(){
-        meme.src="./imgs/meme1.jpg"
+    meme.src="./imgs/meme1.jpeg"
     }
     b4.addEventListener('click', meme1);
-
     let b5 = document.getElementById('meme-2');
     function meme2(){
-        meme.src="./imgs/meme2.jpg"
+    meme.src="./imgs/meme2.jpeg"
     }
     b5.addEventListener('click', meme2);
-
     let b6 = document.getElementById('meme-3');
     function meme3(){
-        meme.src="./imgs/meme3.jpg"
+    meme.src="./imgs/meme3.jpeg"
     }
     b6.addEventListener('click', meme3);
-
     let b7 = document.getElementById('meme-4');
     function meme4(){
-        meme.src="./imgs/meme4.jpg"
+    meme.src="./imgs/meme4.jpeg"
     }
     b7.addEventListener('click', meme4);
-
-
-
     let memeDiv = document.getElementById('meme-image-container');
-    //let rect1 = memeDiv.getBoundingClientRect();
-   
-    
-
-    
-    //alert("Are you ready to generate a awsome meme???")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
