300d29ce2fabd858a37a6a5b8c450f0c0dd50b10
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-07 13:48:20 -0300

    Put JS at page end and remove onload funtion

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-window.onload = function() {
-  const upperTextInput = document.querySelector('#text-input');
-  upperTextInput.addEventListener('input', function(e) {
-    const upperText = document.querySelector('#meme-text');
-    upperText.innerHTML = e.target.value;
-  });
+const upperTextInput = document.querySelector('#text-input');
+upperTextInput.addEventListener('input', function(e) {
+  const upperText = document.querySelector('#meme-text');
+  upperText.innerHTML = e.target.value;
+});
 
-  const lowerTextImput = document.querySelector('#lower-text-imput');
-  lowerTextImput.addEventListener('input', function(e) {
-    const lowerText = document.querySelector('#lower-text');
-    lowerText.innerHTML = e.target.value;
-  });
+const lowerTextImput = document.querySelector('#lower-text-imput');
+lowerTextImput.addEventListener('input', function(e) {
+  const lowerText = document.querySelector('#lower-text');
+  lowerText.innerHTML = e.target.value;
+});
 
-  const memeImageInput = document.querySelector('#meme-insert');
-  const memeImage = document.querySelector('#meme-image');
-  memeImageInput.addEventListener( 'input', function(e) {
-    memeImage.src = window.URL.createObjectURL(e.target.files[0]);
-  });
+const memeImageInput = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
+memeImageInput.addEventListener( 'input', function(e) {
+  memeImage.src = window.URL.createObjectURL(e.target.files[0]);
+});
 
-  const borderBtnOne = document.querySelector('#button1');
-  const borderBtnTwo = document.querySelector('#button2');
-  const borderBtnThree = document.querySelector('#button3');
-  const memeContainer = document.querySelector('#meme-image-container');
-  borderBtnOne.addEventListener('click', function(e) {
-    memeContainer.classList.add('border1');
-    memeContainer.classList.remove('border2');
-    memeContainer.classList.remove('border3');
-  });
-  borderBtnTwo.addEventListener('click', function(e) {
-    memeContainer.classList.remove('border1');
-    memeContainer.classList.add('border2');
-    memeContainer.classList.remove('border3');
-  });
-  borderBtnThree.addEventListener('click', function(e) {
-    memeContainer.classList.remove('border1');
-    memeContainer.classList.remove('border2');
-    memeContainer.classList.add('border3');
-  });
+const borderBtnOne = document.querySelector('#button1');
+const borderBtnTwo = document.querySelector('#button2');
+const borderBtnThree = document.querySelector('#button3');
+const memeContainer = document.querySelector('#meme-image-container');
+borderBtnOne.addEventListener('click', function(e) {
+  memeContainer.classList.add('border1');
+  memeContainer.classList.remove('border2');
+  memeContainer.classList.remove('border3');
+});
+borderBtnTwo.addEventListener('click', function(e) {
+  memeContainer.classList.remove('border1');
+  memeContainer.classList.add('border2');
+  memeContainer.classList.remove('border3');
+});
+borderBtnThree.addEventListener('click', function(e) {
+  memeContainer.classList.remove('border1');
+  memeContainer.classList.remove('border2');
+  memeContainer.classList.add('border3');
+});
 
-  featuredImgs = document.querySelectorAll('.meme-thumb');
-  featuredImgs.forEach((img) => {
-    img.addEventListener('click', function(e) {
-      memeImage.src = img.src;
-    });
+featuredImgs = document.querySelectorAll('.meme-thumb');
+featuredImgs.forEach((img) => {
+  img.addEventListener('click', function(e) {
+    memeImage.src = img.src;
   });
-};
+});
