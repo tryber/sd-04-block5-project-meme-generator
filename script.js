9a57c53bee21bb02907f68660610947753eb9c91
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-07 18:33:16 -0300

    added the buttons for change border of the meme

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 window.onload = function() {
-  
+
 //Select Elements
 const inputText = document.getElementById('text-input');
 const inputImage = document.getElementById('meme-insert');
 const memeImage = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text');
+const memeContainer = document.getElementById('meme-image-container');
+const btn1 = document.getElementById('button1');
+const btn2 = document.getElementById('button2');
+const btn3 = document.getElementById('button3');
+const resetBorder = document.getElementById('reset-btn');
 
 inputImage.addEventListener('input', function() {
   memeImage.src = URL.createObjectURL(this.files[0]);
@@ -14,4 +19,19 @@ inputText.addEventListener('input', function() {
   memeText.innerHTML = inputText.value;
 })
 
+btn1.addEventListener('click', function() {
+  memeContainer.style.border = "3px dashed red";
+})
+
+btn2.addEventListener('click', function() {
+  memeContainer.style.border = "5px double blue";
+})
+
+btn3.addEventListener('click', function() {
+  memeContainer.style.border = "6px groove green"
+})
+
+resetBorder.addEventListener('click', function() {
+  memeContainer.style.border = "1px solid black"
+})
 }
