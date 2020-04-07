4eada4fc4c538cf9f14e5ba9e94798729eadb290
cacobribeiro <cacobribeiro@gmail.com>
2020-04-07 00:35:05 -0300

    Imagem e Text 80% colocados...

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-window.onload(function(){
 
+let insertText = document.getElementById("text-input");
 
-    
+insertText.addEventListener('input', function () {
+    memeText.innerHTML = insertText.value;
 })
+
+let memeInsert = document.getElementById("meme-insert");
+   
+memeInsert.addEventListener('input', function () {
+   memeImage.src = memeInsert.value; 
+    })
