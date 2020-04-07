96791ab2c1f3adaed76ef9e27dd02c9f5b2b0c71
TerezaCarvalho <terezasimoes@gmail.com>
2020-04-07 20:34:19 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 
  
   window.onload = function(){
-   let imageOut = document.getElementById("meme-image"); //id na tag img
-   let usuarioImage = document.getElementById("meme-insert"); // pega a imagem q o usuario inseriu
-      console.log(usuarioImage);
+
+   let imageOut = document.getElementById("meme-image"); 
+   let usuarioImage = document.getElementById("meme-insert"); 
+     
     
    function load_arquivo (){
-    imageOut.src = URL.createObjectURL(usuarioImage.files[0]); // a img que sai na tela recebe a img que o imagemusuario
-  }
+    imageOut.src = URL.createObjectURL(usuarioImage.files[0]); 
+   }
    usuarioImage.addEventListener("input", load_arquivo);
-   // evento input qnd o valor de um elemento input é alterado
+ 
    
 
-  var textoInserido = document.getElementById("text-input"); // pega o texto que o usuario digitou
-  let textofoto = document.getElementById("meme-text"); 
+    var textoInserido = document.getElementById("text-input");
+    let textofoto = document.getElementById("meme-text"); 
 
-  function insereTextoNaFoto(){
-    textofoto.innerHTML=textoInserido.value;
-  }
-  textoInserido.addEventListener("keyup",insereTextoNaFoto);
-  // evento keyup qndo?
+    function insereTextoNaFoto(){
+      textofoto.innerHTML=textoInserido.value;
+    }
+   textoInserido.addEventListener("keyup",insereTextoNaFoto);
+
 
   }
 
