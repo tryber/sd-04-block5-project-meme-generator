18b434ac2296a932bb18ad74da9df3394614932c
frederico-campello <fredfmc33@gmail.com>
2020-04-07 09:30:12 -0300

    Atualizando o projeto

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 window.onload = function () {
-    let inputText = document.querySelector("#text-input");
-    let textMeme = document.querySelector("meme-text");
-    let uploadImage = document.querySelector("#meme-insert");
-    let memeImage = document.querySelector("#meme-image");
+    let inputText = document.querySelector('.text-input');
+    let textMeme = document.querySelector('.meme-text');
+    let uploadImage = document.querySelector('.image-upload');
+    let memeImage = document.querySelector('.meme-image');
 
-    inputText.input = function () {
+    function insereTexto () {
         textMeme.innerHTML = inputText.value
-    };
+    }
+    
+    
+    inputText.addEventListener('input', insereTexto);
 
 }
