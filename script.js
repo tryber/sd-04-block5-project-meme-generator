e76e6ee9f925f641cdd68d4375aa9adfc2c154e4
joaosoutto <joaosoutto92@gmail.com>
2020-04-07 00:30:26 -0300

    tentando requisito das margens (e outras correções no código

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 
-    //função de adicionar o texto!-----------------------------------------
+    //função de adicionar o texto!
     let memeText = document.getElementById('text-input');
     let text = document.getElementById('meme-text');
 
@@ -9,10 +9,9 @@ window.onload = function(){
         };
 
     memeText.addEventListener('input', inputText);
-    //função de adicionar o texto!-----------------------------------------
-    
-
-    //função de adicionar imagem!------------------------------------------
+    //função de adicionar o texto!
+    //-----------------------------------------------------------------------------------------------------
+    //função de adicionar imagem!
     let memeInput = document.getElementById('meme-insert');
     let meme = document.getElementById('meme-image');
     
@@ -21,9 +20,31 @@ window.onload = function(){
         };
 
     memeInput.addEventListener('input', imgInput);
-    //função de adicionar imagem!------------------------------------------
-
+    //função de adicionar imagem!
+    //-----------------------------------------------------------------------------------------------------
+    //função de alterar margens!------------------------------------------
     
+    let b1 = document.getElementById('button1');
+    function border1(){
+        memeDiv.style.border='3px dashed red';
+    }
+    b1.addEventListener('click', border1);
+
+    let b2 = document.getElementById('button2');
+    function border2(){
+        memeDiv.style.border='5px double blue';
+    }
+    b2.addEventListener('click', border2);
+
+    let b3 = document.getElementById('button3');
+    function border3(){
+        memeDiv.style.border='6px groove green';
+    }
+    b3.addEventListener('click', border3);
+
+
+
+
     let memeDiv = document.getElementById('meme-image-container');
     //let rect1 = memeDiv.getBoundingClientRect();
    
