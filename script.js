818b1b597de34cc2aa82db99907ca0fc42073ad3
matheusysd <matheusyurisd@gmail.com>
2020-04-07 01:47:54 -0300

    Buttons added

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ input.addEventListener('change', function(){
     imageMeme.src = reader.result;
   }
 });
+
+let button1 = document.querySelector(".button1");
+button1.addEventListener('click', function(){
+  document.getElementById('meme-image-container').style.borderStyle = "dashed";
+  document.getElementById('meme-image-container').style.borderColor = "red";
+  document.getElementById('meme-image-container').style.borderWidth = "3px";
+});
+
+let button2 = document.querySelector(".button2");
+button2.addEventListener('click', function(){
+  document.getElementById('meme-image-container').style.borderStyle = "double";
+  document.getElementById('meme-image-container').style.borderColor = "blue";
+  document.getElementById('meme-image-container').style.borderWidth = "5px";
+});
+
+let button3 = document.querySelector(".button3");
+button3.addEventListener('click', function(){
+  document.getElementById('meme-image-container').style.borderStyle = "groove";
+  document.getElementById('meme-image-container').style.borderColor = "green";
+  document.getElementById('meme-image-container').style.borderWidth = "6px";
+});
