47013ba49bcf43688f03ca6fffe5041acad8338e
Filipe de Castro <filipeyoga@gmail.com>
2020-04-06 21:14:36 -0300

    bonus phase

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.onload = function(){
-
 let textInput = document.querySelector("#text-input");
 let textMeme = document.querySelector("#text-meme");
-
 let memeImg = document.querySelector("#meme-image");
 let memeInsert = document.querySelector("#meme-insert");
+let container = document.querySelector('#meme-image-container');
 
 memeInsert.addEventListener("change", function () {
   memeImg.src = URL.createObjectURL(this.files[0]);
@@ -14,4 +12,27 @@ textInput.addEventListener("keyup", function () {
   let txt = textInput.value;
   textMeme.innerText = txt.toUpperCase();
 });
-}
+
+let button1 = document.querySelector('#button1');
+button1.style.border = "3px dashed red";
+
+button1.addEventListener('click', e => {
+  e.preventDefault();
+  container.style.border = button1.style.border;
+});
+
+let button2 = document.querySelector('#button2');
+button2.style.border = "5px double blue";
+
+button2.addEventListener('click', e => {
+  e.preventDefault();
+  container.style.border = button2.style.border;
+});
+
+let button3 = document.querySelector('#button3');
+button3.style.border = "6px groove green";
+
+button3.addEventListener('click', e => {
+  e.preventDefault();
+  container.style.border = button3.style.border;
+});
