d5c0831708425e9127093848b53c88ce03914f58
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-06 23:21:53 -0300

    Estrutura, declaraçao de variaveis

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 window.onload = function() {
-
+    let memeInsert = document.querySelector("#meme-insert");
+    let textInput = document.querySelector("#text-input");
+    let memeImageContainer = document.querySelector("#meme-image-container");
+    let memeImage = document.querySelector("#meme-image");
+    let memeText = document.querySelector("#meme-text");
 }
