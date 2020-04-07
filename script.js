7d02fcb0e8cf7dd09882fd22ad8cbc4e18b83ecb
joaosoutto <joaosoutto92@gmail.com>
2020-04-06 22:22:53 -0300

    testes

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 window.onload = function(){
 
+    //função de adicionar o texto!-----------------------------------------
     let memeText = document.getElementById('text-input');
-    let memeDiv = document.getElementById('meme-image-container');
+    let text = document.getElementById('meme-text');
+
     function inputText(){
-        memeDiv.innerText = memeText.value;
-    }
-    memeText.addEventListener('keyup', inputText);
+        text.innerHTML = memeText.value
+        };
+
+    memeText.addEventListener('input', inputText);
+    //função de adicionar o texto!-----------------------------------------
+    
+
+    //função de adicionar imagem!------------------------------------------
+    let memeInput = document.getElementById('meme-insert');
+    let meme = document.getElementById('meme-image');
+    
+    function memeInput(){
+        meme.src = URL.createObjectURL(event.target.files[0]);
+        };
+
+    memeInput.addEventListener('input', memeInput);
+    //função de adicionar imagem!------------------------------------------
+
+    
+    let memeDiv = document.getElementById('meme-image-container');
+    //let rect1 = memeDiv.getBoundingClientRect();
+   
+    
 
     
     //alert("Are you ready to generate a awsome meme???")
