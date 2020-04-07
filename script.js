7a45d16f5714874645e7d3906d06cbb0a7500978
Pedro Pires <pedrotpoliveira@gmail.com>
2020-04-06 23:40:43 -0300

    Cypress check -> Attempt 5

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ window.onload = function () {
     const memeText = document.querySelector("#meme-text");
     memeText.textContent = event.target.value.toUpperCase();
   });
+
+  const imgInput = document.querySelector("#meme-insert");
+  const imgContainer = document.querySelector("#meme-image-container");
+
+  imgInput.addEventListener ("change", (event) => {
+    const newImg = document.createElement("img");
+    newImg.id = "meme-image";
+    newImg.className = "meme-image";
+    newImg.src = URL.createObjectURL(event.target.files[0]);
+    imgContainer.appendChild(newImg);
+  });
 };
