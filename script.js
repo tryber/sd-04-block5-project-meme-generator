25c8702143496175e39f4d322136ca069a54a921
deboracosilveira <deboracosilveira@gmail.com>
2020-04-07 09:28:43 -0300

    test2

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function writeOnImg() {
   let text = document.querySelector('#text-input').value;
-  let imageContainer = document.querySelector('#meme-image-container');
+  let imageContainer = document.querySelector('#meme-text');
   
   imageContainer.innerHTML = text;
   
   }
+
+// let img;
+
+function loadImg(event) {
+  // if (!img) {
+    img = document.createElement("img");
+    // img.id = "meme-image";
+  // }
+  img.src = URL.createObjectURL(event.target.files[0]);
+  document.querySelector("#meme-image-container").appendChild(img);
+}
