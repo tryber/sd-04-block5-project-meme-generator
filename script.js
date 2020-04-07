abd32694ababad44eaaf623cbe915c817a41a342
Fanderson Kaecio <kaecio@yahoo.com.br>
2020-04-07 19:45:05 -0300

    modificando elementos para os requisitos pedidos

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ window.onload = function(){
     if (typeof(Storage)!=="undefined") {
         let textBox = document.getElementById("text-input");
         let arquivo = document.getElementById("meme-insert");
-        let containerImg = document.getElementById("img");
-        let intoText = document.getElementById("text-phrase");
+        let containerImg = document.getElementById("meme-image");
+        let intoText = document.getElementById("meme-text");
      
 
         // criando função inserir o texto
         function inserText() {
-            intoText.innerHTML = textBox.value;
+            intoText.innerText = textBox.value;
         };
 
         // add o texto dentro da caixa de da img
