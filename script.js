122994ff5124dca1940347ef85c4f271d3fd3657
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-06 21:26:19 -0300

    trying to solve cypress episode 1 season 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let memeContainer;
 let img;
-let txts = [];
+let text;
 window.onload = function () {
 	memeContainer = document.getElementById("meme-image-container");
 }
@@ -8,6 +8,7 @@ function addImageToContainer(event) {
 	if (!img) {
 		img = document.createElement("img");
 		img.style.width = "100%";
+		img.className ="meme-image"
 		memeContainer.prepend(img);
 	}
 	img.src = URL.createObjectURL(event.target.files[0]);
@@ -17,10 +18,8 @@ function addImageToContainer(event) {
 }
 
 function addTextToContainer(event, index) {
-	let text = txts[index];
 	if (!text) {
 		text = document.createElement("p");
-		txts[index] = text;
 		memeContainer.appendChild(text);
 	}
 	text.innerHTML = event.target.value;
