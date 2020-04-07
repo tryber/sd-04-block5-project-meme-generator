68997f34bc771b8cad790817f7f0696cc4d56a52
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-06 21:08:01 -0300

    Modificada forma de exibição da imagem, redefinida tag img dentro do index e atribuído um id a ela.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const userImage = document.getElementById('text-input'); //imagem do input file.
 const container = document.getElementById('meme-image-container'); // container da imagem.
 const txt = document.querySelector('div#meme-image-container p'); // tag p da imagem.
-let img; //
+const img = document.getElementById('meme-image'); //
 
 userImage.addEventListener('change', addImage); // evento para o input file.
 
 function addImage(event) {
+    img.src = URL.createObjectURL(event.target.files[0]);
+    img.style.display = 'block';
+}
+
+/*function addImage(event) {
     if(!img){
         img = document.createElement('img');
     } else {
@@ -15,4 +20,5 @@ function addImage(event) {
     img.alt = 'Imagem do meme escolhido';
     img.id = 'meme-image';
     container.appendChild(img);
-}
+    img.style.display = 'block';
+}*/
