1081b8cbc5ef6c70fa413c74a55dee331d57626e
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-07 09:32:27 -0300

    change input text and remove add image

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 window.onload = function(){
     const caixaTexto = document.querySelector('#text-input');
-    const textdisplayed = document.querySelector('#meme-image-container');
+    const textToDisplayContainer = document.querySelector('#meme-text');
     caixaTexto.addEventListener('keyup', trocaTexto);
-
-    function trocaTexto () {
-        textdisplayed.innerText = caixaTexto.value;
-      
+      function trocaTexto () {
+        textToDisplayContainer.innerText = caixaTexto.value;
       } 
+
+      //Add image
+    // const caixaImagem = document.querySelector('#meme-insert')
+    // caixaTexto.addEventListener('keyup', addImageToContainer);
+    // let img;
+    //   function addImageToContainer(event){
+    //   if (!img){
+    //   img = document.createElement("img");
+    //     }
+    //   img.src = "image.jpg";
+    //   //img.src = URL.createObjectURL(event.target.files[0]);
+    //   caixaImagem.appendChild(img);
+      
+    // } 
+   
+
+    // image temp
+    
+    // const caixaImagem = document.querySelector('#meme-insert');
+    // caixaImagem.addEventListener('click', trocaImagem);
+    //
+
+          // Part add image
+      // caixaTexto.addEventListener('keyup', addImageToContainer);
+      // let img;
+      // function addImageToContainer(event){
+      // if (!img){
+      // img = document.createElement("img");
+      // img.style.width = "80%";
+      // }
+      // img.src = "image.jpg";
+      // //img.src = URL.createObjectURL(event.target.files[0]);
+      // memeImageContainer.appendChild(img);
+    //}
+
 }
 
  
