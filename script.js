eb94e3e29a6535dcf1397ce2b8d940fbb52bb97d
Josias Viveiro <josiasviveiro@gmail.com>
2020-04-07 00:33:38 -0300

    Insere texto e imagem

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ window.onload = function() {
     let memeImageContainer = document.querySelector("#meme-image-container");
     let memeImage = document.querySelector("#meme-image");
     let memeText = document.querySelector("#meme-text");
+
+    function insertText() {
+        memeText.innerHTML = textInput.value;
+    }
+
+    function insertImage() {
+        memeImage.src = URL.createObjectURL(memeInsert.files[0]);
+    }
+
+    textInput.addEventListener("input", insertText);
+
+    memeInsert.addEventListener("input", insertImage);
+
 }
