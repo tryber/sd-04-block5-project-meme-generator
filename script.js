1aaebe34787bb09f4854488fd3b12171ad5907d8
deboracosilveira <deboracosilveira@gmail.com>
2020-04-07 11:00:22 -0300

    requirement-2

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
+// 
 function writeOnImg() {
   let text = document.querySelector('#text-input').value;
   let imageContainer = document.querySelector('#meme-text');
-  
+
   imageContainer.innerHTML = text;
   
   }
 
-// let img;
-
-function loadImg(event) {
-  // if (!img) {
-    img = document.createElement("img");
-    // img.id = "meme-image";
-  // }
-  img.src = URL.createObjectURL(event.target.files[0]);
-  document.querySelector("#meme-image-container").appendChild(img);
-}
+let memeInsert = document.querySelector("#meme-insert");
+  memeInsert.addEventListener("input", function() {
+  img = document.querySelector("img");
+  img.id = "meme-image";
+  img.src = URL.createObjectURL(this.files[0]);
+})
