20a9468841b3c58ef88cd8d7f1e4a40dda31e027
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-04-06 22:37:54 -0300

    adicionado event listener que insere a imagem no container correto

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let memeText = document.getElementById("meme-text");
 textInput.addEventListener("keyup", function(){
   memeText.innerHTML = textInput.value;
 });
+let memeInsert = document.getElementById("meme-insert");
+let memeImage = document.getElementById("meme-image");
+memeInsert.addEventListener("input", function(){
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+});
