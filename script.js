4c036cd599a3ec24a1945874e7ca77485705ad1f
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-07 12:24:16 -0300

    Add lower text feature

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ window.onload = function() {
     upperText.innerHTML = e.target.value;
   });
 
+  const lowerTextImput = document.querySelector('#lower-text-imput');
+  lowerTextImput.addEventListener('input', function(e) {
+    const lowerText = document.querySelector('#lower-text');
+    lowerText.innerHTML = e.target.value;
+  });
+
   const memeImageInput = document.querySelector('#meme-insert');
   const memeImage = document.querySelector('#meme-image');
-
   memeImageInput.addEventListener( 'input', function(e) {
     memeImage.src = window.URL.createObjectURL(e.target.files[0]);
   });
