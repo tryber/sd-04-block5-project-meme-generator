b60fc03d2134ef50921f8c39afb715ff551cc7f5
Gregor H. Andrade <hammerschmittgregor2@gmail.com>
2020-04-06 22:29:37 -0300

    make image clickable

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-window.onload = function () {
+
     let memeText = document.getElementById("meme-text")
+    let memeImage = document.getElementById("meme-image")
     let textInput = document.getElementById("text-input")
     let imageInput = document.getElementById("meme-insert")
     let imageContainer = document.getElementById("meme-image-container")
     let buttom1 = document.getElementById("button1")
     let buttom2 = document.getElementById("button2")
     let buttom3 = document.getElementById("button3")
+    let image1 = document.getElementById("meme-1")
+    let image2 = document.getElementById("meme-2")
+    let image3 = document.getElementById("meme-3")
+    let image4 = document.getElementById("meme-4")
 
     imageInput.addEventListener("input", function(e){
-        let memeImage = document.createElement("img")
         memeImage.src = URL.createObjectURL(e.target.files[0])
-        memeImage.id = "meme-image"
-        memeImage.className = "meme-image"
-        imageContainer.append(memeImage)
-
     })
-
     textInput.addEventListener("keyup", function(){
         memeText.innerText = textInput.value
     })
-
     buttom1.addEventListener("click", function(){
         imageContainer.style.border = "dashed 3px red"
     })
@@ -29,4 +27,7 @@ window.onload = function () {
     buttom3.addEventListener("click", function(){
         imageContainer.style.border = "groove 6px green"
     })
+
+function changeImage(image){
+    memeImage.src = image;
 }
