edc93dfcd197899f5c1b0b774c92a41ff6879766
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-07 10:54:27 -0300

    step2

--- script.js
+++ script.js
@@ -5,5 +5,23 @@ window.onload = function () {
   // input text dynamically over the image
   inputText.addEventListener("input", () => { text.innerHTML = inputText.value })
 
+  const memeInsert = this.document.getElementById("meme-insert");
+  const container = this.document.getElementById("meme-image-container");
+  const image = this.document.getElementById("meme-image");
+
+  memeInsert.addEventListener("change", function() {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      image.style.display = "block";
+      reader.addEventListener("load", function() {
+        //atribuindo o valor de src do FileReader através do this.result
+        image.setAttribute("src", this.result)
+      })
+      reader.readAsDataURL(file);
+    }
+
+        
+  })
 }
 
