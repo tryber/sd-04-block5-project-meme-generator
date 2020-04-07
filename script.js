094b30c3fa52791f0ec9103eacbe470d452f49f3
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-07 01:57:39 -0300

    Limitada a quantidade de caracteres do input text para 60.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const inputImage = document.getElementById('meme-insert'); //imagem do input file.
-const inputText = document.getElementById('text-input');
+const inputImage = document.getElementById('meme-insert'); // Manipula o Input type="file".
+const inputText = document.getElementById('text-input'); // Manipula o a caixa para inserir texto.
 const container = document.getElementById('meme-image-container'); // container da imagem.
-const txt = document.getElementById('meme-text'); // tag p da imagem.
-const img = document.getElementById('meme-image'); //
+const txt = document.getElementById('meme-text'); // Manipula tag p do texto da imagem.
+const img = document.getElementById('meme-image'); // Manipula a imagem.
+const btn1 = document.getElementById('button1');
+const btn2 = document.getElementById('button2');
+const btn3 = document.getElementById('button3');
 
 inputImage.addEventListener('change', addImage); // evento para o input file.
 inputText.addEventListener('keyup', addText);
+btn1.addEventListener('click', mudaBorda);
+btn2.addEventListener('click', mudaBorda);
+btn3.addEventListener('click', mudaBorda);
 
 function addImage() {
     img.src = URL.createObjectURL(event.target.files[0]);
@@ -16,6 +22,17 @@ function addImage() {
 function addText() {
     let memeTxt = inputText.value;
     txt.innerText = memeTxt;
+}
+
+function mudaBorda(event) {
+    let tipo = event.target.id;
+    if(tipo === 'button1') {
+        container.style.border = '3px dashed red';
+    } else if(tipo === 'button2') {
+        container.style.border = '5px double blue';
+    } else {
+        container.style.border = '6px groove green';
+    }
 }
 
 /*function addImage(event) {
