f99c7718774d2fcd2a306791a8024ae21aeb2982
danilossousa <danilosantossousa@gmail.com>
2020-04-07 09:39:28 -0300

    Teste 2 bonus 5

--- script.js
+++ script.js
@@ -10,45 +10,13 @@ window.onload = function () {
   let meme3 = document.getElementById("meme-3");
   let meme4 = document.getElementById("meme-4");
   let memeImg = document.getElementById("meme-image");
-
-  function pasteTxt() {
-    memeTxt.innerText = inpTxt.value;
-  };
-
-  function borderStyle1() {
-    container.style.border = "3px dashed red";
-  };
-
-  function borderStyle2() {
-    container.style.border = "5px double blue";
-  };
-
-  function borderStyle3() {
-    container.style.border = "6px groove green";
-  };
-
-  function imgContainer1() {
-    memeImg.src = "imgs/meme1.jpeg";
-  };
-
-  function imgContainer2() {
-    memeImg.src = "imgs/meme2.jpeg";
-  };
-
-  function imgContainer3() {
-    memeImg.src = "imgs/meme3.jpeg";
-  };
-
-  function imgContainer4() {
-    memeImg.src = "imgs/meme4.jpeg";
-  };
-
-  inpTxt.addEventListener("input", pasteTxt);
-  but1.addEventListener("click", borderStyle1);
-  but2.addEventListener("click", borderStyle2);
-  but3.addEventListener("click", borderStyle3);
-  meme1.addEventListener("click", imgContainer1);
-  meme2.addEventListener("click", imgContainer2);
-  meme3.addEventListener("click", imgContainer3);
-  meme4.addEventListener("click", imgContainer4);
+  inpTxt.addEventListener("input", function () {memeTxt.innerText = inpTxt.value;});
+  but1.addEventListener("click", function () {container.style.border = "3px dashed red";});
+  but2.addEventListener("click", function () {container.style.border = "5px double blue";});
+  but3.addEventListener("click", function () {container.style.border = "6px groove green";});
+  meme1.addEventListener("click", function () {memeImg.src = "imgs/meme1.jpeg";});
+  meme2.addEventListener("click", function () {memeImg.src = "imgs/meme2.jpeg";});
+  meme3.addEventListener("click", function () {memeImg.src = "imgs/meme3.jpeg";});
+  meme4.addEventListener("click", function () {memeImg.src = "imgs/meme4.jpeg";});
 };
+
