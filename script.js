89e0f46832752affb6fc8f87b15f8b6f5d78442e
cacobribeiro <cacobribeiro@gmail.com>
2020-04-07 18:12:27 -0300

    Fix 3.3 - added buttons

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ image.addEventListener("change", function () {
     memeImage.src = URL.createObjectURL(this.files[0]);
 })
 
+let container = document.querySelector("#meme-image-container");
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
+
+button1.addEventListener("click", function (){
+    container.style.border = "3px dashed rgb(255, 0 , 0)"
+})
+
+button2.addEventListener("click", function (){
+    container.style.border = "5px double rgb(0, 0, 255)"
+})
+
+button3.addEventListener("click", function (){
+    container.style.border = "6px groove rgb(0, 255, 0)"
+})
