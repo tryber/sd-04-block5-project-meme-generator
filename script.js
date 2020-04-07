6ecd87e6f3d26498efb57d487132aa80de5baa31
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-07 12:35:27 -0300

    Added the window.onload function and the functions to input text and image

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+window.onload = function() {
+
+
 //Select Elements
-const inputText = document.getElementById('input-text');
+const inputText = document.getElementById('text-input');
 const inputImage = document.getElementById('meme-insert');
 const memeImage = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text');
+
+inputImage.addEventListener('input', function() {
+  memeImage.src = URL.createObjectURL(this.files[0]);
+})
+
+inputText.addEventListener('input', function() {
+  memeText.innerHTML = inputText.value;
+})
+
+}
