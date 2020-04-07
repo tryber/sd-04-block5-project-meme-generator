772fe86bff64d691606bfd1f29213d5f446fadc9
jessica-tavares <jessicatavareseel@gmail.com>
2020-04-07 09:11:26 -0300

    sem o to upper case e com meme-text

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ window.onload = function (){
   let img = document.getElementById('meme-image');
   let text = document.getElementById('text-input');
   text.addEventListener('change',function(){
-    let caps = text.value.toLocaleUpperCase();
-    document.getElementById('text').innerHTML = caps;
+    let caps = text.value;
+    document.getElementById('meme-text').innerHTML = caps;
   })
   let image = document.getElementById('meme-insert');
   image.addEventListener("change",save)
