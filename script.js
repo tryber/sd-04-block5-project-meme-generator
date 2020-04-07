471c56dadad1b52c24d02e105c4a26c4d62e8e89
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-06 22:18:27 -0300

    text-meme to meme-text

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
 let memeImg = document.querySelector('#meme-image');
 let memeInsert = document.querySelector('#meme-insert');
 let textInput = document.querySelector('#text-input');
-let textMeme = document.querySelector('#text-meme');
+let textMeme = document.querySelector('#meme-text');
 
 let button1 = document.querySelector('#button1');
 let button2 = document.querySelector('#button2');
 let button3 = document.querySelector('#button3');
 
 let container = document.querySelector("#meme-image-container");
-
 
 memeInsert.addEventListener("change", function(){
   memeImg.src = URL.createObjectURL(this.files[0]);
