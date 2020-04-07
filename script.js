9bcd92811273351fb2b1699997f15891ec483353
juniomelos <juniomelos@gmail.com>
2020-04-06 22:12:25 -0300

    add border

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 window.onload = function(){
 
-  let textInput = document.querySelector('#text-input');
-  let imgInput = document.querySelector('#meme-insert');
-  let container = document.querySelector('#meme-image-container');
-  let memeText = document.querySelector('#meme-text');
-
-  textInput.addEventListener('keyup', function(){
-    memeText.innerHTML = textInput.value;
+  let textInput = document.getElementById('text-input');
+  let text = document.getElementById('meme-text');
+  
+  textInput.addEventListener('input', function(){
+      text.innerHTML = textInput.value }
+  )
+  
+  let imgInput = document.getElementById('meme-insert');
+  let img = document.getElementById('meme-image');
+  
+  imgInput.addEventListener('input', function(){
+      console.log(imgInput.files[0].name);
+      img.src = URL.createObjectURL(event.target.files[0]);
   });
 
-  function addImage(event){
-    let image = document.createElement('img');
-    image.id = "meme-image";
-    image.src = URL.createObjectURL(event.target.files[0]);
-    container.appendChild(image);
-  };
-  imgInput.addEventListener('change', addImage);
+  let frameImg = document.getElementById("meme-image-container");
+
+  let btn1 = document.getElementById("button1");
+  btn1.addEventListener("click", function(){
+    frameImg.style.border = "3px dashed red";
+  });
+  let btn2 = document.getElementById("button2");
+  btn2.addEventListener("click", function(){
+    frameImg.style.border = "5px double blue";
+  });
+  let btn2 = document.getElementById("button3");
+  btn2.addEventListener("click", function(){
+    frameImg.style.border = "6px groove green";
+  });
 }
