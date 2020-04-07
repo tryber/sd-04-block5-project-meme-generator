7f6a7b12b5752660ecb4306dbc3d7ce2fcfa29a1
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-07 11:02:38 -0300

    add meme-image permanent

--- script.js
+++ script.js
@@ -11,43 +11,21 @@ window.onload = function(){
     const memeinsertvalue = document.querySelector('#meme-insert');
     const buttonevent = document.querySelector('#buttonLoadImage');
     
-    onchange
-    //memeinsertvalue.addEventListener('input.onchange', addImageToContainer);
     buttonevent.addEventListener('click', addImageToContainer);
     let img;
     let imgpath;
       function addImageToContainer(event){
       if (!img){
       img = document.createElement("img");
-      img.id = "meme-image";
+      img.id = "meme-image"
         }
       imgpath = memeinsertvalue.files[0].name;
       console.log(imgpath);
       img.src = imgpath;
-      //img.src = URL.createObjectURL(event.target.files[0]);
       caixaImagem.appendChild(img);
       
     } 
-   
-
-    // image temp
-    
-    // const caixaImagem = document.querySelector('#meme-insert');
-    // caixaImagem.addEventListener('click', trocaImagem);
-    //
-
-          // Part add image
-      // caixaTexto.addEventListener('keyup', addImageToContainer);
-      // let img;
-      // function addImageToContainer(event){
-      // if (!img){
-      // img = document.createElement("img");
-      // img.style.width = "80%";
-      // }
-      // img.src = "image.jpg";
-      // //img.src = URL.createObjectURL(event.target.files[0]);
-      // memeImageContainer.appendChild(img);
-    //}
+  
 
 }
 
