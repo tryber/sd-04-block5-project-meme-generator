588ce90df6d75a68eba5acbd1297bf1e6226e662
Diego Matheus Campos <diegodiko05@gmail.com>
2020-04-06 21:51:56 -0300

    Bonus 4

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+
 let button = document.querySelector('#upload-image');
 let memeImg = document.querySelector('#meme-image');
 let memeInsert = document.querySelector('#meme-insert');
 let textInput = document.querySelector('#text-input');
 let textMeme = document.querySelector('#text-meme');
 
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+
+let container = document.querySelector("#meme-image-container");
+
 
 memeInsert.addEventListener("change", function(){
   memeImg.src = URL.createObjectURL(this.files[0]);
-})
+});
 
 textInput.addEventListener('keyup', function(){
-  textMeme.innerText = textInput.value
-})
+  textMeme.innerText = textInput.value;
+});
+
+button1.addEventListener('click', function(){
+  container.style.border = "3px dashed red";
+});
+
+button2.addEventListener('click', function(){
+  container.style.border = "5px double blue";
+});
+
+button3.addEventListener('click', function(){
+  container.style.border = "6px groove green";
+});
