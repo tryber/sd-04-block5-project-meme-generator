0c12432e1f9aed7ecf125d9ed5cc908347d1d62f
Eduardo Santos <eduardo.debest81@gmail.com>
2020-04-07 13:50:52 -0300

    code climate fix

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ let memeContainer;
 let img;
 let text;
 window.onload = function () {
-	memeContainer = document.getElementById("meme-image-container");
-	img = document.getElementById("meme-image");
-	text = document.getElementById("meme-text");
-	memeContainer.style.height = img.clientHeight + "px";
+  memeContainer = document.getElementById("meme-image-container");
+  img = document.getElementById("meme-image");
+  text = document.getElementById("meme-text");
+  memeContainer.style.height = img.clientHeight + "px";
 }
 function addImageToContainer(event) {
-	loadImage(URL.createObjectURL(event.target.files[0]));
+  loadImage(URL.createObjectURL(event.target.files[0]));
 }
 
 function addTextToContainer(event, index) {
-	text.innerHTML = event.target.value;
+  text.innerHTML = event.target.value;
 }
 
 function changeTextColor(event) {
-	text.style.color = event.target.value;
+  text.style.color = event.target.value;
 }
 
 function loadImage(imgPath) {
-	img.src = imgPath;
-	img.onload = function () {
-		memeContainer.style.height = img.clientHeight + "px";
-	}
+  img.src = imgPath;
+  img.onload = function () {
+    memeContainer.style.height = img.clientHeight + "px";
+  }
 }
 
 function changeBorder(size, style, color) {
-	memeContainer.style.border = `${size} ${style} ${color}`;
+  memeContainer.style.border = `${size} ${style} ${color}`;
 }
