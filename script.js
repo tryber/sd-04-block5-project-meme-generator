5db7c498f8c9cb25ce41e36766d563a00f00c0f9
cacobribeiro <cacobribeiro@gmail.com>
2020-04-07 17:32:25 -0300

    Fix 2.4

--- script.js
+++ script.js
@@ -6,19 +6,8 @@ insertText.addEventListener('input', function () {
 })
 
 let image = document.querySelector("#meme-insert");
-image.addEventListener("input", function () {
-    image = sessionStorage.setItem("image", image.value);
-    document.querySelector("#meme-image").src = sessionStorage.image;
+image.addEventListener("change", function () {
+    let memeImage = document.querySelector("#meme-image");
+    memeImage.src = URL.createObjectURL(this.files[0]);
 })
 
-
-// let memeInsert = document.getElementById("meme-insert");
-   
-// memeInsert.addEventListener('input', function () {
-//     let image = localStorage.setItem("image", memeInsert.value); 
-    
-
-// let createImagTag = document.createElement("img");
-// createImagTag.src = image;
-// image = document.getElementById("meme-image-container").appendChild(createImagTag);
-// })
