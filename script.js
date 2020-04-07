fe35ed93c6a58378f47359302e89f0360dff5af7
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-06 23:09:00 -0300

    Reconfigurado posicionamente da imagem e do texto.

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const inputImage = document.getElementById('meme-insert'); //imagem do input file.
+const inputText = document.getElementById('text-input');
 const container = document.getElementById('meme-image-container'); // container da imagem.
 const txt = document.getElementById('meme-text'); // tag p da imagem.
 const img = document.getElementById('meme-image'); //
 
 inputImage.addEventListener('change', addImage); // evento para o input file.
-
+inputText.addEventListener('keyup', addText);
 
 function addImage() {
     img.src = URL.createObjectURL(event.target.files[0]);
     container.appendChild(img);
     img.style.display = 'block';
+    txt.style.display = 'block';
+}
+
+function addText() {
+    let memeTxt = inputText.value;
+    txt.innerText = memeTxt;
 }
 
 /*function addImage(event) {
