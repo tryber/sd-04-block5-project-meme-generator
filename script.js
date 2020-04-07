d03750969369aa370dff47604487f7c018f75cb9
cacobribeiro <cacobribeiro@gmail.com>
2020-04-07 16:11:29 -0300

    Foto e Texto fixed? maybe

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ insertText.addEventListener('input', function () {
     memeText.innerHTML = insertText.value;
 })
 
-let memeInsert = document.getElementById("meme-insert");
+let image = document.querySelector("#meme-insert");
+image.addEventListener("input", function () {
+    image = sessionStorage.setItem("image", image.value);
+    let createdImagTag = document.createElement("img");
+    createdImagTag.src = sessionStorage.image;
+    document.querySelector("#meme-image-container").appendChild(createdImagTag);  
+})
+
+
+// let memeInsert = document.getElementById("meme-insert");
    
-memeInsert.addEventListener('input', function () {
-   memeImage.src = memeInsert.value; 
-    })
+// memeInsert.addEventListener('input', function () {
+//     let image = localStorage.setItem("image", memeInsert.value); 
+    
+
+// let createImagTag = document.createElement("img");
+// createImagTag.src = image;
+// image = document.getElementById("meme-image-container").appendChild(createImagTag);
+// })
