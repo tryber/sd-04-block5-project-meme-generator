d888dd0fb44a6c6c310a6b6f4477029165e48eb4
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-07 11:52:53 -0300

    HTML struture created and Elements selected in javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+//Select Elements
+const inputText = document.getElementById('input-text');
+const inputImage = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
+const memeText = document.getElementById('meme-text');
