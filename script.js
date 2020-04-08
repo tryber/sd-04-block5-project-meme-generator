33cfb819c947bad6718af440c033604fa6026b8b
Ana Higo <anahigo.arq@gmail.com>
2020-04-08 13:52:14 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+- Função para carregar a imagem e para o texto aparecer em cima da imagem;
+*/
+window.onload = function() {
+    let memeImage = document.getElementById("meme-image"); 
+    let memeInsert = document.getElementById("meme-insert"); 
+    
+    function uploadImage(){
+        memeImage.src = URL.createObjectURL(memeInsert.files[0]); 
+    }
+    memeInsert.addEventListener("input", uploadImage);
+
+    let textInput = document.getElementById("text-input");
+    let memeText = document.getElementById("meme-text"); 
+ 
+    function textImage(){
+        memeText.innerHTML=textInput.value;
+    }
+    textInput.addEventListener("keyup",textImage);
+}
