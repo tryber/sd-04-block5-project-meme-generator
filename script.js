d2181db79d5c5893dcd2065e5231dc869f4ab2b9
Victor L <victor.lustosacp@gmail.com>
2020-04-08 15:49:05 -0300

    project

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let inpFile = document.getElementById('inpFile');
-let previewContainer = document.getElementById('imagePreview');
+let previewContainer = document.getElementById('meme-image-container');
 let previewImage = previewContainer.querySelector('.image-preview__image');
 let previewDefaultText = document.getElementById('image-preview__defaut-text');
-let text = document.getElementById('txt');
+let text = document.getElementById('text-input');
 let button = document.getElementById('button');
-inpFile.addEventListener('change',function(){
+button.addEventListener('click',function(){
     var fReader = new FileReader();
     fReader.readAsDataURL(inpFile.files[0]);
     fReader.onloadend = function(event){
     previewImage.src = event.target.result;
-    }
-})
-button.addEventListener('click',function(){
     previewDefaultText.innerHTML = String(text.value);
-    
+    }
 })
