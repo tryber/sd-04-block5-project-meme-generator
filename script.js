3af522790618f0d6549904e0e5c48633b239f859
AliceAAtalla <aliceatalla36@gmail.com>
2020-04-08 17:45:07 -0300

    Requisitos 1-3 - test 1

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ window.onload = function () {
   let memeContainer = document.querySelector("#meme-image-container");
   let uploadImg = document.getElementById("meme-insert");
   let imageMeme = document.getElementById("meme-image");
+  let inpText = document.getElementById("text-input");
+  let text = document.getElementById("meme-text");
 
   uploadImg.addEventListener("input", function () {
     imageMeme.src = URL.createObjectURL(event.target.files[0]);
   });
+  inpText.addEventListener("input", function () {
+    text.innerHTML = inpText.value;
+  });
+  
 };
