a28ab59d6dc0adb4bd057d5cd195aa1979ffcac4
Victor L <victor.lustosacp@gmail.com>
2020-04-08 15:28:25 -0300

    project

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let inpFile = document.getElementById('inpFile');
 let previewContainer = document.getElementById('imagePreview');
 let previewImage = previewContainer.querySelector('.image-preview__image');
-let previewDefaultText = previewContainer.querySelector('image-preview__default-text');
-
+let previewDefaultText = document.getElementById('image-preview__defaut-text');
+let text = document.getElementById('txt');
+let button = document.getElementById('button');
 inpFile.addEventListener('change',function(){
     var fReader = new FileReader();
     fReader.readAsDataURL(inpFile.files[0]);
     fReader.onloadend = function(event){
-    
     previewImage.src = event.target.result;
-    console.log(event.target.result);
     }
+})
+button.addEventListener('click',function(){
+    previewDefaultText.innerHTML = String(text.value);
+    
 })
