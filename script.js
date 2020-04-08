53ffd1c7846600a9c3016f5c5712acd932dfeb13
AliceAAtalla <aliceatalla36@gmail.com>
2020-04-08 12:18:17 -0300

    Upload Imagem

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-window.onload = function {
+window.onload = function () {
+  let memeContainer = document.querySelector("#meme-image-container");
+  let uploadImg = document.getElementById("meme-insert");
+  let imageMeme = document.getElementById("meme-image");
 
-
-
-
-  
-}
+  uploadImg.addEventListener("input", function () {
+    imageMeme.src = URL.createObjectURL(event.target.files[0]);
+  });
+};
