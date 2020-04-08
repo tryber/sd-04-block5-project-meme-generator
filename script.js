c0876b83a060f6259989dbb1fe5394de59fee48d
wistyk <leonardoxw@gmail.com>
2020-04-07 21:28:22 -0300

    she done already done had herses

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function texto() {
   let text = document.querySelector('#text-input')
-  let texDig = document.querySelector('.meme-text').innerText = text.value.toUpperCase()
+  let texDig = document.querySelector('#meme-text').innerText = text.value.toUpperCase()
 }
 
 let carregar = function(event) {
-let image = document.querySelector('.meme-image');
+let image = document.querySelector('#meme-image');
 image.src = URL.createObjectURL(event.target.files[0]);
 }
