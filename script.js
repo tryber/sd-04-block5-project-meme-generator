b1ef66810e192f6464408c6234d9c25ca80ea2f3
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-04-08 20:00:13 -0300

    feature/added buttons to change border

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-let textInput = document.querySelector('#text-input');
-let imageContainer = document.querySelector('#meme-image-container');
-let memeText = document.querySelector('#meme-text');
-let memeInsert = document.querySelector('#meme-insert');
+// Query selectors
+let textInput = document.querySelector('.text-input');
+let imageContainer = document.querySelector('.meme-image-container');
+let memeText = document.querySelector('.meme-text');
+let memeInsert = document.querySelector('.meme-insert');
+let button1 = document.querySelector('.button1');
+let button2 = document.querySelector('.button2');
+let button3 = document.querySelector('.button3');
 
+// Text input
 textInput.addEventListener('keyup', () => {
   memeText.innerHTML = textInput.value;
 });
 
+// Image upload
 memeInsert.addEventListener('change', () => {
   let img = document.createElement('img');
   img.id = 'meme-image';
   img.alt = 'meme image';
   imageContainer.appendChild(img);
   img.src = URL.createObjectURL(event.target.files[0]);
+});
+
+// Border buttons
+button1.addEventListener('click', () => {
+  imageContainer.style.border = '3px dashed red';
+});
+
+button2.addEventListener('click', () => {
+  imageContainer.style.border = '5px double blue';
+});
+
+button3.addEventListener('click', () => {
+  imageContainer.style.border = '6px groove green';
 });
