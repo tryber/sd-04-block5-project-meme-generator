737b0c9af2695507700eafa289209d7940befb2e
AliceAAtalla <aliceatalla36@gmail.com>
2020-04-09 14:36:59 -0300

    Teste 1

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 window.onload = load;
 
-function load() {
-  let memeContainer = document.querySelector("#meme-image-container");
-  let uploadImg = document.getElementById("meme-insert");
-  let imageMeme = document.getElementById("meme-image");
-  let inpText = document.getElementById("text-input");
-  let text = document.getElementById("meme-text");
-  let btn1 = document.getElementById("button1");
-  let btn2 = document.getElementById("button2");
-  let btn3 = document.getElementById("button3");
-  let reset = document.getElementById("buttonReset");
-  let memes = document.getElementsByClassName("memeMiniatura");
+let memeContainer = document.querySelector("#meme-image-container");
+let uploadImg = document.getElementById("meme-insert");
+let imageMeme = document.getElementById("meme-image");
+let inpText = document.getElementById("text-input");
+let text = document.getElementById("meme-text");
+let btn1 = document.getElementById("button1");
+let btn2 = document.getElementById("button2");
+let btn3 = document.getElementById("button3");
+let reset = document.getElementById("buttonReset");
+let memes = document.getElementsByClassName("memeMiniatura");
 
+function load() {
+  console.log(uploadImg);
   uploadImg.addEventListener("input", function () {
     imageMeme.src = URL.createObjectURL(event.target.files[0]);
   });
-
   inpText.addEventListener("input", function textImg() {
     text.innerHTML = inpText.value;
   });
-
   btn1.addEventListener("click", function () {
     memeContainer.style.border = "3px dashed red";
   });
