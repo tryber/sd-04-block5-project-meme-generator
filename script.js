d4b3067949fb184ee03965680f71cbabd180ee9a
Gabriel Almeida <gabriellukke@pop-os.localdomain>
2020-04-09 17:31:10 -0300

    Fix code climate last 2 issues

--- script.js
+++ script.js
@@ -9,27 +9,26 @@ const btn2 = document.getElementById('button2');
 const btn3 = document.getElementById('button3');
 const resetBorder = document.getElementById('reset-btn');
 const memes = document.getElementsByClassName('meme');
-inputImage.addEventListener('input', function() {
-  memeImage.src = URL.createObjectURL(this.files[0]);
-})
+
+inputImage.addEventListener('input', function(){
+  memeImage.src = URL.createObjectURL(this.files[0]);});
+
 inputText.addEventListener('input', function() {
-  memeText.innerHTML = inputText.value;
-})
+  memeText.innerHTML = inputText.value;})
+
 btn1.addEventListener('click', function() {
-  memeContainer.style.border = "3px dashed red";
-})
+  memeContainer.style.border = "3px dashed red";})
+
 btn2.addEventListener('click', function() {
-  memeContainer.style.border = "5px double blue";
-})
+  memeContainer.style.border = "5px double blue";})
+
 btn3.addEventListener('click', function() {
-  memeContainer.style.border = "6px groove green"
-})
+  memeContainer.style.border = "6px groove green"})
+
 resetBorder.addEventListener('click', function() {
-  memeContainer.style.border = "1px solid black"
-})
+  memeContainer.style.border = "1px solid black"})
+
 for (meme of memes) {
   meme.addEventListener('click', function(event) {
-    memeImage.src = event.target.src;
-  })
-}
+    memeImage.src = event.target.src;})}
 }
