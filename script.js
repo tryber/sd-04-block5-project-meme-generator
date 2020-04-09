5b502215ce4c4d18bb51651f4112c492fdfa7236
Douglas Costa <douglaza@gmail.com>
2020-04-09 12:11:07 -0300

    changing script and tags

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-let insertMeme = document.getElementById("meme-insert");
-let imageMeme = document.getElementById("meme-image");
-let inputText = document.getElementById("text-input");
-let textMeme = document.getElementById("meme-text");
+let insertMeme = document.querySelector("#meme-insert");
+let imageMeme = document.querySelector("#meme-image");
+let inputText = document.querySelector("#text-input");
+let textMeme = document.querySelector("#meme-text");
 
-
-insertMeme.addEventListener('input', function() {
-    imageMeme.src = URL.createObjectURL(this.files[0]);
+insertMeme.addEventListener('change', function () {
+  imageMeme.src = URL.createObjectURL(this.files[0]);
 });
 
-inputText.addEventListener('input', function(){
-    textMeme.innerHTML = inputText.value;
+inputText.addEventListener('input', function () {
+  textMeme.innerHTML = inputText.value;
 });
