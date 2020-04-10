c7593bebd1fda54e9ebb35dce2e3708658954784
Julianafatsil <julianafatsil@gmail.com>
2020-04-10 20:33:55 -0300

    Javascript structure change

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let textInput = document.getElementById("text-input");
+let memeImage = document.querySelector("input[type='file']");
+
 function readImage(input) {
     if (input.files && input.files[0]) {
         let reader = new FileReader();
@@ -14,13 +17,10 @@ function readTextImage(input) {
     document.getElementById('meme-text').innerText = input.value
 }
 
-const memeImage = document.querySelector("input[type='file']");
-
 memeImage.addEventListener('change', function () {
     readImage(this);
 });
 
-const textInput = document.getElementById("text-input");
 textInput.addEventListener('keypress', function () {
     readTextImage(this);
 });
