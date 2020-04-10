22a6935db6af01a6c5ae25257b54ce9b6aa28eb8
Julianafatsil <julianafatsil@gmail.com>
2020-04-10 02:08:56 -0300

    Create Function readImage

---
+++ script.js
@@ -0,0 +1,11 @@
+function readImage(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('meme-image')
+                .src = e.target.result;
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
