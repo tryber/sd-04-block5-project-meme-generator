78133a9e2db4cd03ae47016f7c809e08eb9585e4
Julianafatsil <julianafatsil@gmail.com>
2020-04-10 18:14:59 -0300

    removed the onchenge and onkeypress properties from within HTML into JAVASCRIPT using addEventListner

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ function readImage(input) {
     }
 }
 
-function readTextImage(){
-    document.getElementById('meme-text').innerText = document.getElementById('text-input').value
+function readTextImage(input) {
+    document.getElementById('meme-text').innerText = input.value
 }
+
+const memeImage = document.querySelector("input[type='file']");
+
+memeImage.addEventListener('change', function () {
+    readImage(this);
+});
+
+const textInput = document.getElementById("text-input");
+textInput.addEventListener('keypress', function () {
+    readTextImage(this);
+});
+textInput.addEventListener('change', function() {
+    readTextImage(this);
+});
