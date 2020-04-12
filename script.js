b84e295f69e5dd8ee1c4a1e22ae156b4100d8226
wistyk <leonardoxw@gmail.com>
2020-04-12 14:18:43 -0300

    refeito partes do html, css e js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function texto() {
   let text = document.querySelector('#text-input');
   let texDig = document.querySelector('#meme-text');
 
   text.addEventListener('keyup', function() {
     texDig.innerText = this.value.toUpperCase();
   })
-}
 
 let carregar = function(event) {
 let image = document.querySelector('#meme-image');
