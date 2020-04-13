25ab89221f207d3c64cdd0a11fc4f73237281c19
djonata-dias <diasdjonata@gmail.com>
2020-04-13 17:02:01 -0300

    posição e tamanho da imagem

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 window.onload = function () {
-  let imageInsert = document.getElementById("meme-insert");
+  let memeInsert = document.getElementById("meme-insert");
   let memeImage = document.getElementById("meme-image");
   let textInput = document.getElementById("text-input");
   let textImage = document.getElementById("meme-text");
   let memeContainer = document.getElementById("meme-image-container");
 
   function loadImage() {
-    let url = URL.createObjectURL(imageInsert.files[0]);
-    memeImage.setAttribute("src", url);
+    memeImage.src = URL.createObjectURL(memeInsert.files[0]);
   }
   function loadText() {
     textImage.innerHTML = textInput.value;
   }
 
   textInput.addEventListener("input", loadText);
-  imageInsert.addEventListener("input", loadImage);
+  memeInsert.addEventListener("input", loadImage);
 };
