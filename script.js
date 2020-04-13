562dac4ef218f2a219d9daa737310af523a0afe5
wistyk <leonardoxw@gmail.com>
2020-04-12 21:42:21 -0300

    teste

---
+++ script.js
@@ -0,0 +1,11 @@
+let text = document.querySelector('#text-input');
+let texDig = document.querySelector('#meme-text');
+
+text.addEventListener('keyup', function() {
+  texDig.innerText = this.value.toUpperCase();
+})
+
+let carregar = function(event) {
+let image = document.querySelector('#meme-image');
+image.src = URL.createObjectURL(event.target.files[0]);
+}
