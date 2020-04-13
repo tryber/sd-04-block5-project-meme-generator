fea1c93effc7877fe6c88fac27a902803a623611
wistyk <leonardoxw@gmail.com>
2020-04-13 11:23:47 -0300

    requisito 1 com javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const textoDe = document.querySelector('#text-input');
+const textoPara = document.querySelector('#meme-text');
+
+textoDe.addEventListener('keyup', function () {
+textoPara.innerText = this.value
+})
