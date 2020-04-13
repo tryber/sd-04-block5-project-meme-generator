6e12cfbd15bbe2921160540317b5131d19c68b22
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-04-13 16:36:00 -0300

    add 3 buttons to change one div border

--- script.js
+++ script.js
@@ -27,3 +27,27 @@ window.onload = function(){
     caixaImagem.appendChild(img);
   }
 }
+//button 1 change border style
+const button1event = document.querySelector('#button1');
+button1event.addEventListener('click',button1changestyle);
+	function button1changestyle (){
+		document.getElementById("meme-image-container").style.borderColor = "red";
+		document.getElementById("meme-image-container").style.borderStyle = "dashed";
+		document.getElementById("meme-image-container").style.borderWidth = '3px';
+	}
+//button 2 change border style
+const button2event = document.querySelector('#button2');
+button2event.addEventListener('click',button2changestyle);
+	function button2changestyle (){
+		document.getElementById("meme-image-container").style.borderColor = "blue";
+		document.getElementById("meme-image-container").style.borderStyle = "double";
+		document.getElementById("meme-image-container").style.borderWidth = '5px';
+	}
+	//button 3 change border style
+const button3event = document.querySelector('#button3');
+button3event.addEventListener('click',button3changestyle);
+	function button3changestyle (){
+		document.getElementById("meme-image-container").style.borderColor = "green";
+		document.getElementById("meme-image-container").style.borderStyle = "groove";
+		document.getElementById("meme-image-container").style.borderWidth = '6px';
+	}
