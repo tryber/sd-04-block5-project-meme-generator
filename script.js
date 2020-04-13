3e9032d776d29d2883d5e519172dcd61fbf26a2d
lucasokazava <lucasokazava@gmail.com>
2020-04-13 17:16:22 -0300

    second requisite + exchanging querySelector for getElementById

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const caixaDeTexto = document.querySelector("#text-input");
-const caixaDaImagem = document.querySelector("#meme-image-container");
-const texto = document.querySelector("#meme-text");
+window.onload = function () {
+    const caixaDeTexto = document.getElementById("text-input");
+    const caixaDaImagem = document.getElementById("meme-image-container");
+    const texto = document.getElementById("meme-text");
+    const botaoUpload = document.getElementById("meme-insert");
+    const imagem = document.getElementById("meme-image");
 
-function inserirTexto () {
-    texto.innerText = caixaDeTexto.value;
+    function inserirTexto () {
+        texto.innerText = caixaDeTexto.value;
+    }
+    function uploadImagem () {
+        imagem.src = URL.createObjectURL(botaoUpload.files[0]);
+    }
+
+    caixaDeTexto.addEventListener('keyup', inserirTexto);
+    botaoUpload.addEventListener('change', uploadImagem);
 }
-
-caixaDeTexto.addEventListener('keyup', inserirTexto);
