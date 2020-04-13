e583626cc931cff060c43add5a73035332fb624a
djonata-dias <diasdjonata@gmail.com>
2020-04-13 16:40:06 -0300

    consertando a borda

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function () {
-  let imageInsert = this.document.getElementById("meme-insert");
-  let memeImage = this.document.getElementById("meme-image");
-  let textInput = this.document.getElementById("text-input");
-  let textImage = this.document.getElementById("meme-text");
-  let memeContaine = this.document.getElementById("meme-image-container");
+  let imageInsert = document.getElementById("meme-insert");
+  let memeImage = document.getElementById("meme-image");
+  let textInput = document.getElementById("text-input");
+  let textImage = document.getElementById("meme-text");
+  let memeContainer = document.getElementById("meme-image-container");
 
   function loadImage() {
     let url = URL.createObjectURL(imageInsert.files[0]);
