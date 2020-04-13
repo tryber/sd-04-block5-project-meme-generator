08fc8fbb7f5c08d2f308700d6a3509aaf95e685a
wistyk <leonardoxw@gmail.com>
2020-04-13 16:24:49 -0300

    removido a função para carregar a imagem no js

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ const textoPara = document.querySelector('#meme-text');
 textoDe.addEventListener('keyup', function () {
 textoPara.innerText = this.value
 })
-
-let carregar = function(event) {
-  let image = document.querySelector('#meme-image');
-  image.src = URL.createObjectURL(event.target.files[0]);
-}
