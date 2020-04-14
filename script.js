a84392bf3ca7e3929d76c0475478f1b4b64ad640
wistyk <leonardoxw@gmail.com>
2020-04-14 17:34:39 -0300

    bonus4

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ let carregar = function(event) {
   let image = document.querySelector('#meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
 }
+
+let bUm = document.querySelector('#button1');
+let bDois = document.querySelector('#button2');
+let bTres = document.querySelector('#button3');
+let container = document.querySelector('#meme-image-container')
+
+bUm.addEventListener('click', function () {
+  container.classList.add('button1');
+})
+
+bDois.addEventListener('click', function () {
+  container.classList.add('button2');
+})
+
+bTres.addEventListener('click', function () {
+  container.classList.add('button3');
+})
