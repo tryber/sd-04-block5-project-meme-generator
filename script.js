267ec8a51086e154eab6c250e45ca9050116ac31
deboracosilveira <deboracosilveira@gmail.com>
2020-04-14 09:59:57 -0300

    bonus-1(4)

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ let memeInsert = document.querySelector("#meme-insert");
   img.id = "meme-image";
   img.src = URL.createObjectURL(this.files[0]);
 })
+// Variables
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let container = document.getElementById("meme-image-container");
+// Button 1 - border type
+button1.addEventListener("click", function(){
+  container.style.border = "3px dashed red";
+})
+// Button 2 - border type
+button2.addEventListener("click", function(){
+  container.style.border = "5px double blue";
+})
+// Button 3 - border type
+button3.addEventListener("click", function(){
+  container.style.border = "6px groove green";
+})
