d227841dab3779929e25d27060228d723d727514
wistyk <leonardoxw@gmail.com>
2020-04-14 20:08:00 -0300

    bonus5

--- script.js
+++ script.js
@@ -13,16 +13,36 @@ let carregar = function(event) {
 let bUm = document.querySelector('#button1');
 let bDois = document.querySelector('#button2');
 let bTres = document.querySelector('#button3');
-let container = document.querySelector('#meme-image-container')
+let container = document.querySelector('#meme-image-container');
 
 bUm.addEventListener('click', function () {
   container.classList.add('button1');
-})
+});
 
 bDois.addEventListener('click', function () {
   container.classList.add('button2');
-})
+});
 
 bTres.addEventListener('click', function () {
   container.classList.add('button3');
-})
+});
+
+let botImgUm = document.querySelector('#meme-1');
+botImgUm.addEventListener('click', function () {
+  let image = document.querySelector('#meme-image').src='meme1.jpeg';
+});
+
+let botImgDois = document.querySelector('#meme-2');
+botImgDois.addEventListener('click', function () {
+  let image = document.querySelector('#meme-image').src='meme2.jpeg';
+});
+
+let botImgTres = document.querySelector('#meme-3');
+botImgTres.addEventListener('click', function () {
+  let image = document.querySelector('#meme-image').src='meme3.jpeg';
+});
+
+let botImgQuatro = document.querySelector('#meme-4');
+botImgQuatro.addEventListener('click', function () {
+  let image = document.querySelector('#meme-image').src='meme4.jpeg';
+});
