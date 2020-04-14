80503afe165f1eae94e02a2826572540044a6143
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-14 16:50:42 -0300

    Tentativa requisitos bônus

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 function submitText() {
-  let text = document.getElementById("meme-text");
-  let textInput = document.getElementById("text-input").value;
+  let text = document.getElementById('meme-text');
+  let textInput = document.getElementById('text-input').value;
   text.innerHTML = textInput;
 }
 
-document.getElementById("text-input").addEventListener('input', submitText);
+document.getElementById('text-input').addEventListener('input', submitText);
 
-function addImage(event) {
-  let img = document.getElementById("meme-image");
-  img.src = URL.createObjectURL(event.target.files[0]);
-}
+let memeImage = document.getElementById('meme-image');
+let memeInput = document.getElementById('meme-insert');
+memeInput.addEventListener('input', function(){
+  memeImage.src = URL.createObjectURL(this.files[0]);
+});
+
+let button1 = document.getElementById("button1");
+button1.addEventListener('click', function() {
+  let imageContainer = document.getElementById("meme-image-container");
+  imageContainer.className = 'meme-image-container selectedOne';
+});
+
+let button2 = document.getElementById("button2");
+button2.addEventListener('click', function() {
+  let imageContainer = document.getElementById("meme-image-container");
+  imageContainer.className = 'meme-image-container selectedTwo';
+});
+
+let button3 = document.getElementById("button3");
+button3.addEventListener('click', function() {
+  let imageContainer = document.getElementById("meme-image-container");
+  imageContainer.className = 'meme-image-container selectedThree';
+});
