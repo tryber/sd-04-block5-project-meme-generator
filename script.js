b2fc9461dba0648beab84ca6367d7c8aaa877364
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-04-14 16:24:11 -0300

    Novos requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function submitText() {
-  let text = document.getElementById('meme-text');
-  let textInput = document.getElementById('text-input').value;
+  let text = document.getElementById("meme-text");
+  let textInput = document.getElementById("text-input").value;
   text.innerHTML = textInput;
 }
 
-document.getElementById('text-input').addEventListener('input', submitText);
+document.getElementById("text-input").addEventListener('input', submitText);
 
-let memeImage = document.getElementById('meme-image');
-let memeInput = document.getElementById('meme-insert');
-memeInput.addEventListener('input', function(){
-  memeImage.src = URL.createObjectURL(this.files[0]);
-});
+function addImage(event) {
+  let img = document.getElementById("meme-image");
+  img.src = URL.createObjectURL(event.target.files[0]);
+}
