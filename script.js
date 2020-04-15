0f2b41328f12a410970b52ebbdf00f3df6ad5176
Caio Magalhaes <caio.lmf@gmail.com>
2020-04-15 12:44:49 -0300

    Do code refactoring for best pratices and performance

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
 const upperTextInput = document.querySelector('#text-input');
+const lowerTextImput = document.querySelector('#lower-text-imput');
+const memeImageInput = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
+const borderBtnOne = document.querySelector('#button1');
+const borderBtnTwo = document.querySelector('#button2');
+const borderBtnThree = document.querySelector('#button3');
+const memeContainer = document.querySelector('#meme-image-container');
+const featuredImgs = document.querySelectorAll('.meme-thumb');
+
 upperTextInput.addEventListener('input', (e) => {
   const upperText = document.querySelector('#meme-text');
   upperText.innerHTML = e.target.value;
 });
 
-const lowerTextImput = document.querySelector('#lower-text-imput');
 lowerTextImput.addEventListener('input', (e) => {
   const lowerText = document.querySelector('#lower-text');
   lowerText.innerHTML = e.target.value;
 });
 
-const memeImageInput = document.querySelector('#meme-insert');
-const memeImage = document.querySelector('#meme-image');
 memeImageInput.addEventListener('input', (e) => {
   memeImage.src = window.URL.createObjectURL(e.target.files[0]);
 });
 
-const borderBtnOne = document.querySelector('#button1');
-const borderBtnTwo = document.querySelector('#button2');
-const borderBtnThree = document.querySelector('#button3');
-const memeContainer = document.querySelector('#meme-image-container');
 borderBtnOne.addEventListener('click', () => {
-  memeContainer.classList.add('border1');
-  memeContainer.classList.remove('border2');
-  memeContainer.classList.remove('border3');
+  changeBorder(memeContainer, 1);
 });
 borderBtnTwo.addEventListener('click', () => {
-  memeContainer.classList.remove('border1');
-  memeContainer.classList.add('border2');
-  memeContainer.classList.remove('border3');
+  changeBorder(memeContainer, 2);
 });
 borderBtnThree.addEventListener('click', () => {
-  memeContainer.classList.remove('border1');
-  memeContainer.classList.remove('border2');
-  memeContainer.classList.add('border3');
+  changeBorder(memeContainer, 3);
 });
 
-const featuredImgs = document.querySelectorAll('.meme-thumb');
 featuredImgs.forEach((img) => {
   img.addEventListener('click', () => {
     memeImage.src = img.src;
   });
 });
+
+function changeBorder(
+  element = null,
+  borderStyle = null,
+) {
+  element.classList.remove('border1');
+  element.classList.remove('border2');
+  element.classList.remove('border3');
+
+  switch (borderStyle) {
+    case 1:
+      element.classList.add('border1');
+      break;
+    case 2:
+      element.classList.add('border2');
+      break;
+    case 3:
+      element.classList.add('border3');
+      break;
+    default:
+      break;
+  }
+}
