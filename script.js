3ec4f5b2403304e51aef3633ebd4cb1b9e22a458
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-04-18 12:57:51 -0300

    visando requisitos bonus

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
+
 var textInput = document.querySelector("#text-input");
 var textDisplay = document.querySelector("#meme-text");
 var imageDisplay = document.querySelector("#meme-image");
 
-document.getElementById('meme-insert').addEventListener("input", handle);
+var button1 = document.querySelector("#button1");
+var button2 = document.querySelector("#button2");
+var button3 = document.querySelector("#button3");
 
-textInput.addEventListener("input", insertText)
+var image1 = document.getElementById("preLoaded1");
+var image2 = document.getElementById("preLoaded2");
+var image3 = document.getElementById("preLoaded3");
+var image4 = document.getElementById("preLoaded4");
+
+var container = document.querySelector("#meme-image-container");
+
+document.getElementById('meme-insert').addEventListener("input", handle);
+textInput.addEventListener("input", insertText);
+button1.addEventListener("click", addClassB1);
+button2.addEventListener("click", addClassB2);
+button3.addEventListener("click", addClassB3);
+image1.addEventListener("click", img1Insert);
+image2.addEventListener("click", img2Insert);
+image3.addEventListener("click", img3Insert);
+image4.addEventListener("click", img4Insert);
+
+function img1Insert() {
+  imageDisplay.src = "./imgs/meme1.png"
+}
+function img2Insert() {
+  imageDisplay.src = "./imgs/meme2.webp"
+}
+function img3Insert() {
+  imageDisplay.src = "./imgs/meme3.png"
+}
+function img4Insert() {
+  imageDisplay.src = "./imgs/meme4.png"
+}
 
 function insertText() {
   textDisplay.innerHTML = textInput.value
@@ -25,4 +56,32 @@ function handle(evt) {
     // '" title="', escape(files[0].name), '"/>'].join('');
   }
   reader.readAsDataURL(files[0]);
+}
+
+function addClassB1(){
+  if(container.classList.contains("button2")){
+    container.classList.remove("button2");
+  }
+  if(container.classList.contains("button3")){
+    container.classList.remove("button3");
+  }
+  container.classList.add("button1");
+}
+function addClassB2(){
+  if(container.classList.contains("button1")){
+    container.classList.remove("button1");
+  }
+  if(container.classList.contains("button3")){
+    container.classList.remove("button3");
+  }
+  container.classList.add("button2");
+}
+function addClassB3(){
+  if(container.classList.contains("button2")){
+    container.classList.remove("button2");
+  }
+  if(container.classList.contains("button1")){
+    container.classList.remove("button1");
+  }
+  container.classList.add("button3");
 }
