fd548a6aa4d5a9477e03d8a01277456e712a0f21
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-04-20 13:58:00 -0300

    refactoring & codeclimate fixes

--- script.js
+++ script.js
@@ -1,48 +1,43 @@
-window.onload = function () {
-  let text = document.getElementById("meme-text");
 
-  let inputText = document.getElementById("text-input");
-  // input text dynamically over the image
+const text = document.getElementById("meme-text");
+const inputText = document.getElementById("text-input");
+const memeInsert = document.getElementById("meme-insert");
+const container = document.getElementById("meme-image-container");
+const image = document.getElementById("meme-image");
+const borderSize = [3, 5, 6];
+const borderStyle = ["dashed", "double", "groove"];
+const borderColor = ["red", "blue", "green"];
+const thumbDiv = this.document.querySelector('.thumbnailsContainer')
+
+window.onload = function () {
+  // input text dynamically over the image:
   inputText.addEventListener("input", function () {
     text.innerHTML = inputText.value
   })
 
-  const memeInsert = this.document.getElementById("meme-insert");
-  const container = this.document.getElementById("meme-image-container");
-  const image = this.document.getElementById("meme-image");
-
-  memeInsert.addEventListener("change", uploudImage)
-  function uploudImage() {
-    const file = this.files[0];
-    if (file) {
-      const reader = new FileReader();
-      image.style.display = "block";
-      reader.addEventListener("load", function () {
-        //atribuindo o valor de src do FileReader através do this.result
-        image.setAttribute("src", this.result)
-      })
-      reader.readAsDataURL(file);
-    }
+  //calling upload function:
+  memeInsert.addEventListener("input", uploudImage)
+  function uploudImage(e) {
+    image.src = URL.createObjectURL(e.target.files[0])
   }
-  let borderSize = [3, 5, 6];
-  let borderStyle = ["dashed", "double", "groove"];
-  let borderColor = ["red", "blue", "green"];
-
-  for (let i = 0; i < borderSize.length; i += 1) {
-
+  //calling change border function for each button:
+  for (let i = 0; i < 3; i += 1) {
     document.getElementById(`button${i + 1}`).addEventListener("click", changeBorder);
-
     function changeBorder() {
       container.style.border = `${borderSize[i]}px ${borderStyle[i]} ${borderColor[i]}`;
     }
   }
 
-  for (let i = 1; i <= 4; i += 1) {
+  //calling thumbToContainer function for each thumb image:
+  thumbDiv.addEventListener('click', thumbToContainer)
+}
 
-    document.getElementById(`meme-${i}`).addEventListener("click", thumbToContainer);
-
-    function thumbToContainer() {
-      image.src = document.getElementById(`meme-${i}`).src;
-    }
+function thumbToContainer(e) {
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  if (e.target && e.target.nodeName == "IMG") {
+    image.src = e.target.src;
+    e.target.classList.add('selected');
   }
 }
