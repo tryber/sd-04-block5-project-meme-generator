12b9f06e150701a054abc76d30049ad8cd6a67a8
jessica-tavares <jessicatavareseel@gmail.com>
2020-04-19 21:06:05 -0300

    testando css changes

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-window.onload = function (){
-  //Coletando as variáveis de entrada:
-  let img = document.getElementById('meme-image');
-  let text = document.getElementById('text-input');
-  let container = document.getElementById('meme-image-container')
-  text.addEventListener('keyup',function(){
-    let caps = text.value;
-    document.getElementById('meme-text').innerHTML = caps;
-  })
-  let image = document.getElementById('meme-insert');
-  image.addEventListener("change",save)
-  function save(){
-    img.src = URL.createObjectURL(image.files[0]);
-  }
-  const button1 = document.getElementById('button1');
-  button1.addEventListener("click", function () {
-    container.style.border = '';
-    container.className = 'button1';
-  })
-
-  const button2 = document.getElementById('button2');
-  button2.addEventListener("click", function () {
-    container.style.border = '';
-    container.className = 'button2';
-  })
-
-  const button3 = document.getElementById('button3');
-  button3.addEventListener("click", function () {
-    container.style.border = '';
-    container.className = 'button3';
-  })
+//Coletando as variáveis de entrada:
+let img = document.getElementById('meme-image');
+let text = document.getElementById('text-input');
+let container = document.getElementById('meme-image-container')
+text.addEventListener('keyup',function(){
+  let caps = text.value;
+  document.getElementById('meme-text').innerHTML = caps;
+})
+let image = document.getElementById('meme-insert');
+image.addEventListener("change",save)
+function save(){
+  img.src = URL.createObjectURL(image.files[0]);
 }
+const button1 = document.getElementById('button1');
+button1.addEventListener("click", function () {
+  container.style.border = '';
+  container.className = 'button1';
+})
+
+const button2 = document.getElementById('button2');
+button2.addEventListener("click", function () {
+  container.style.border = '';
+  container.className = 'button2';
+})
+
+const button3 = document.getElementById('button3');
+button3.addEventListener("click", function () {
+  container.style.border = '';
+  container.className = 'button3';
+})
