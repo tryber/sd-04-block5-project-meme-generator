4b569b2d1397681f0e7ad04a4d26f9103b3aae23
djonata-dias <diasdjonata@gmail.com>
2020-04-21 00:41:06 -0300

    adicionando função dos botões de memes personalizados

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ window.onload = function () {
   let button2 = document.getElementById("button2");
   let button3 = document.getElementById("button3");
   let form = document.querySelector("form");
+  let memeButton1 = document.getElementById("meme-1");
+  let memeButton2 = document.getElementById("meme-2");
+  let memeButton3 = document.getElementById("meme-3");
+  let memeButton4 = document.getElementById("meme-4");
+
   function loadImage() {
     memeImage.src = URL.createObjectURL(memeInsert.files[0]);
   }
@@ -32,6 +37,18 @@ window.onload = function () {
     (memeContainer.style.borderStyle = "groove"),
       (memeContainer.style.borderWidth = "6px");
     memeContainer.style.borderColor = "green";
+  });
+  memeButton1.addEventListener("click", () => {
+    memeImage.src = memeButton1.src;
+  });
+  memeButton2.addEventListener("click", () => {
+    memeImage.src = memeButton2.src;
+  });
+  memeButton3.addEventListener("click", () => {
+    memeImage.src = memeButton3.src;
+  });
+  memeButton4.addEventListener("click", () => {
+    memeImage.src = memeButton4.src;
   });
   form.addEventListener("submit", (e) => {
     e.preventDefault();
