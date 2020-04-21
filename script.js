c98ba583a97f63d0fa6813ee10d841abfefda0ce
djonata-dias <diasdjonata@gmail.com>
2020-04-20 22:51:56 -0300

    Adionado a primeira função do primeiro botão de estilização de borda

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 window.onload = function () {
   let memeInsert = document.getElementById("meme-insert");
   let memeImage = document.getElementById("meme-image");
+  let memeContainer = document.getElementById("meme-image-container")
   let textInput = document.getElementById("text-input");
   let textImage = document.getElementById("meme-text");
-
+  let button1 = document.getElementById("button1");
+  let button2 = document.getElementById("button2");
+  let button3 = document.getElementById("button3");
+  let form = document.querySelector("form");
   function loadImage() {
     memeImage.src = URL.createObjectURL(memeInsert.files[0]);
   }
@@ -13,6 +17,14 @@ window.onload = function () {
   }
 
   textInput.addEventListener("input", loadText);
-
   memeInsert.addEventListener("input", loadImage);
+  button1.addEventListener("click", () => {
+    memeContainer.style.borderStyle = "dashed",
+    memeContainer.style.borderWidth = "3px"
+    memeContainer.style.borderColor = "red"
+  })
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  })
 };
+
