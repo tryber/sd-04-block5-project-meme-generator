caadc1c767c2e805970decffe31ad0d311f6857c
djonata-dias <diasdjonata@gmail.com>
2020-04-20 23:03:15 -0300

    Botões de estilização de borda adicionados e testados

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   let memeInsert = document.getElementById("meme-insert");
   let memeImage = document.getElementById("meme-image");
-  let memeContainer = document.getElementById("meme-image-container")
+  let memeContainer = document.getElementById("meme-image-container");
   let textInput = document.getElementById("text-input");
   let textImage = document.getElementById("meme-text");
   let button1 = document.getElementById("button1");
@@ -19,12 +19,21 @@ window.onload = function () {
   textInput.addEventListener("input", loadText);
   memeInsert.addEventListener("input", loadImage);
   button1.addEventListener("click", () => {
-    memeContainer.style.borderStyle = "dashed",
-    memeContainer.style.borderWidth = "3px"
-    memeContainer.style.borderColor = "red"
-  })
+    (memeContainer.style.borderStyle = "dashed"),
+      (memeContainer.style.borderWidth = "3px");
+    memeContainer.style.borderColor = "red";
+  });
+  button2.addEventListener("click", () => {
+    (memeContainer.style.borderStyle = "double"),
+      (memeContainer.style.borderWidth = "5px");
+    memeContainer.style.borderColor = "blue";
+  });
+  button3.addEventListener("click", () => {
+    (memeContainer.style.borderStyle = "groove"),
+      (memeContainer.style.borderWidth = "6px");
+    memeContainer.style.borderColor = "green";
+  });
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-  })
+  });
 };
-
